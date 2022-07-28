c707d55f1094c597964060cd2b39e2a4fd061cc4
Niall Maher <niall@codu.ie>
2022-07-28 12:54:09 +0100

    Add Fathom Analytics (#20)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,35 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as Fathom from "fathom-client";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+
+  useEffect(() => {
+    const env = process.env.NODE_ENV;
+    const siteId = process.env.NEXT_PUBLIC_FATHOM_SITE_ID;
+
+    if (env !== "production" || !siteId) return;
+
+    Fathom.load(siteId, {
+      includedDomains: ["codu.co"],
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    // Record a pageview when route changes
+    router.events.on("routeChangeComplete", onRouteChangeComplete);
+
+    // Unassign event listener
+    return () => {
+      router.events.off("routeChangeComplete", onRouteChangeComplete);
+    };
+  }, []);
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
