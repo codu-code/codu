e4488130b77805ff6f2b6667035dc4fac4fbe767
Niall Maher <niall@codu.ie>
2022-08-20 11:51:35 +0100

    Revert "[WIP DO NOT MERGE] Accounts setup (#41)" (#42)
    This reverts commit b94a112c3aa16e2b0d272c36225efca40416c21e.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,8 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 import * as Fathom from "fathom-client";
-import { SessionProvider } from "next-auth/react";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { withTRPC } from "@trpc/next";
-import superjson from "superjson";
-import { AppRouter } from "../server/trpc/router";
-
-const getBaseUrl = () => {
-  if (typeof window !== "undefined") return ""; // browser should use relative url
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
-
-  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
-};
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -40,19 +29,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     };
   }, []);
 
-  return (
-    <SessionProvider session={pageProps.session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
-export default withTRPC<AppRouter>({
-  config() {
-    return {
-      url: `${getBaseUrl()}/api/trpc`,
-      transformer: superjson,
-    };
-  },
-  ssr: false,
-})(MyApp);
+export default MyApp;
