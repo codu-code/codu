fe19e309b1a389eae2e27c86b8d235f3d8792bb0
VSB14 <103755444+VSB14@users.noreply.github.com>
2022-10-28 17:08:24 +0530

    Fixed padding issues with the author information.

--- components/BioBar/BioBar.tsx
+++ components/BioBar/BioBar.tsx
@@ -15,8 +15,8 @@ const BioFooter = ({ author }: Props) => {
 
   const { name, image, bio, username } = author;
   return (
-    <div className="max-w-xl px-4 mx-auto  text-gray-700 dark:text-gray-300 mt-6">
-      <div className="flex mx-2 sm:mx-6 md:mx-auto px-4 border-t-2 pt-6 border-gray-300 dark:border-gray-800">
+    <div className="max-w-xl mx-auto  text-gray-700 dark:text-gray-300 mt-6">
+      <div className="flex mx-2 sm:mx-6 md:mx-auto border-t-2 pt-6 border-gray-300 dark:border-gray-800">
         <div className="mr-4 flex-shrink-0 self-center">
           <Image
             className="rounded-full"
