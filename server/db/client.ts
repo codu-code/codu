2382abe3f2636d3993117d40de657d52abbbe37e
Niall Maher <nialljoemaher@gmail.com>
2023-01-23 18:46:39 +0000

    Fix global prisma and logging

--- server/db/client.ts
+++ server/db/client.ts
@@ -5,10 +5,17 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-export default global.prisma ||
+const prisma =
+  global.prisma ||
   new PrismaClient({
     log:
       process.env.NODE_ENV === "development"
         ? ["query", "error", "warn"]
         : ["error"],
   });
+
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
+}
+
+export default prisma;
