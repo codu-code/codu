19dc6eb9618439b9975fb75aaf0427bd97386fc3
Niall Maher <niall@codu.ie>
2023-01-23 07:36:04 +0000

    conditionally add s3 credentials for local dev

--- utils/s3helpers.ts
+++ utils/s3helpers.ts
@@ -1,11 +1,17 @@
 import { S3Client } from "@aws-sdk/client-s3";
 
+const hasAccessKeys = process.env.ACCESS_KEY && process.env.SECRET_KEY;
+
 export const s3Client = new S3Client({
   region: "eu-west-1",
-  // credentials: {
-  //   accessKeyId: process.env.ACCESS_KEY || "",
-  //   secretAccessKey: process.env.SECRET_KEY || "",
-  // },
+  ...(hasAccessKeys
+    ? {
+        credentials: {
+          accessKeyId: process.env.ACCESS_KEY || "",
+          secretAccessKey: process.env.SECRET_KEY || "",
+        },
+      }
+    : {}),
 });
 
 export const uploadFile = async (signedUrl: string, file: File) => {
