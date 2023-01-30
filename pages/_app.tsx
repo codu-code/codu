5395ff194afe32238cb469a4a574b2a759ae1a92
Niall Maher <nialljoemaher@gmail.com>
2023-01-30 17:19:50 +0000

    Cleanup analytics debugging

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,10 +16,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     const env = process.env.NODE_ENV;
     const siteId = process.env.NEXT_PUBLIC_FATHOM_SITE_ID;
 
-    if (env !== "production" || !siteId) {
-      console.error(siteId, "not found", env);
-      return;
-    }
+    if (env !== "production" || !siteId) return;
 
     Fathom.load(siteId, {
       includedDomains: ["codu.co"],
