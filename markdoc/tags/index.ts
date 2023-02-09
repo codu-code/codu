d7263f37288e63306b375a934a3bcb8e0869970d
Son Nguyen <son@sonng.dev>
2023-02-09 22:55:33 +0000

    Support CodePen in article markdown (#156)
    * feat: Create CodePen embed component
    
    * feat: Support `codepen` tag in markdoc
    
    * feat: Use p tag instead of iframe in CodePen
    
    * feat: Support `height` prop in CodePen
    
    * fix: Correct title of CodePen
    
    * feat: Change default height of CodePen embed
    
    * feat: Only require `src` from `codepen` tag
    
    * feat: Change the default tab value of CodePen
    
    * fix: Remove legacy prop in iframe
    
    See https://stackoverflow.com/a/75065655/9744063

--- markdoc/tags/index.ts
+++ markdoc/tags/index.ts
@@ -1,4 +1,5 @@
 module.exports['markdoc-example'] = markdocExample;
+export { default as codepen } from './codepen.markdoc';
 export { default as youtube } from './youtube.markdoc';
 export { default as codesandbox } from './codesandbox.markdoc';
 import markdocExample from './markdoc-example.markdoc';
