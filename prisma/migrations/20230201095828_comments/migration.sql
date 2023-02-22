fabfe3f3d6cd9476dd1caebe9cef5fa9a13ca00f
Niall Maher <niall@codu.ie>
2023-02-22 09:33:40 +0000

    Remove test migrations

--- prisma/migrations/20230201095828_comments/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Comment" ADD COLUMN     "deleted" TIMESTAMP(3);
--- prisma/migrations/20230201185627_comments/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `deleted` on the `Comment` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Comment" DROP COLUMN "deleted";
