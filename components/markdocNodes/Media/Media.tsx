cb07b182bdea323e2e4e634799beb6a71bc15e46
Daniel Callaghan <dan_cal89@hotmail.com>
2023-03-05 00:33:26 +0000

    feat(media tag): Create Media tag that will check embed for url and call appropriate tag to handle the media type - Create a custom media node that allows us to merge embeds #158

---
+++ components/markdocNodes/Media/Media.tsx
@@ -0,0 +1,94 @@
+import * as React from 'react';
+import { YouTube } from '../Youtube/Youtube';
+import { CodePen } from '../Codepen/CodePen';
+import { CodeSandbox } from '../Codesandbox/CodeSandbox';
+import { FallbackMedia } from '../FallbackMedia/FallbackMedia';
+
+const protocolRegex = /^(f|ht)tps?:\/\//i
+const youtubeRegex = /^(?:https?:\/\/)?(?:www\.)?(?:m\.)?(?:youtu\.be\/|youtube\.com\/(?:watch\?(?:feature=youtu\.be[&amp;]?|v=)|embed\/|v\/))([\w-]{11})(?:\S+)?$/;
+const codesandboxRegex = /{%\s*media\s*src="https?:\/\/codesandbox\.io\/embed\/([\w-]+).*?"\s*%}/i;
+const codepenRegex = /{%\s*media\s*src="https?:\/\/codepen\.io\/([\w-]+)\/embed\/([\w-]+).*?"\s*%}/i;
+
+export function Media(props: React.ReactPropTypes) {
+
+    const [type, setType] = React.useState('fallback')
+
+
+    const processMedia = (mediaProps) => {
+        let match;
+        if ((match = mediaProps.src.match(youtubeRegex))) {
+          return processYoutube({...mediaProps});
+        } else if ((match = mediaProps.src.match(codesandboxRegex))) {
+          return processCodeSandbox({...mediaProps});
+        } else if ((match = mediaProps.src.match(codepenRegex))) {
+          return processCodePen({...mediaProps});
+        } else {
+          return processFallback({...mediaProps});
+        }
+      };
+      
+      const processYoutube = (mediaProps) => {
+        let src = checkProtocol(mediaProps.src)
+        return {
+            ...mediaProps,
+            src,
+        }
+
+    }
+    
+      
+      const processCodeSandbox = (mediaProps) => {
+        let src = checkProtocol(mediaProps.src)
+        setType('codesandbox')
+        return {
+            ...mediaProps,
+            src,
+        }
+
+      }
+      
+      const processCodePen = (mediaProps) => {
+        let src = checkProtocol(mediaProps.src)
+        setType('codepen')
+        return {
+            ...mediaProps,
+            src,
+        }
+
+      }
+      
+      const processFallback = (mediaProps) => {
+        const src = checkProtocol(mediaProps.src);
+        setType('fallback')
+        return {
+            ...mediaProps,
+            src,
+        }
+      };
+
+    const renderMediaComponent = (type: string, props: any) => {
+        switch(type) {
+          case 'youtube':
+            return <YouTube {...props} />;
+          case 'codepen':
+            return <CodePen {...props} />;
+          case 'codesandbox':
+            return <CodeSandbox {...props} />;
+          default:
+            return <FallbackMedia {...props} />;
+        }
+      }
+    
+      const checkProtocol = (url: string) => {
+        if(!protocolRegex.test(url)){
+            url = `https://${url}`
+        }
+        return url
+      }
+    
+
+    
+    const processedProps = processMedia(props)
+    console.log(processedProps)
+    return renderMediaComponent(type, processedProps)
+}
