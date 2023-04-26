39f2f7166c8db17df829a25c763da18bff0ffe40
Niall Maher <nialljoemaher@gmail.com>
2023-04-26 21:39:24 +0100

    Remove extra VPC settings

--- cdk/lib/storage-stack.ts
+++ cdk/lib/storage-stack.ts
@@ -21,22 +21,7 @@ export class StorageStack extends cdk.Stack {
     super(scope, id, props);
 
     this.vpc = new ec2.Vpc(this, "StorageStackVpc", {
-      cidr: "10.0.0.0/16",
-      maxAzs: 3,
       natGateways: 1,
-      natGatewaySubnets: { subnetGroupName: "Public" },
-      subnetConfiguration: [
-        {
-          cidrMask: 24,
-          name: "Public",
-          subnetType: ec2.SubnetType.PUBLIC,
-        },
-        {
-          cidrMask: 24,
-          name: "Private",
-          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
-        },
-      ],
     });
 
     const { vpc } = this;
