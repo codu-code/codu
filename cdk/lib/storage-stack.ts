3a4764f47062ee7a728553c6509fc6003a7322a2
Niall Maher <nialljoemaher@gmail.com>
2023-04-26 21:24:41 +0100

    Configure natGateways to 1

--- cdk/lib/storage-stack.ts
+++ cdk/lib/storage-stack.ts
@@ -20,7 +20,25 @@ export class StorageStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: Props) {
     super(scope, id, props);
 
-    this.vpc = new ec2.Vpc(this, "StorageStackVpc");
+    this.vpc = new ec2.Vpc(this, "StorageStackVpc", {
+      cidr: "10.0.0.0/16",
+      maxAzs: 3,
+      natGateways: 1,
+      natGatewaySubnets: { subnetGroupName: "Public" },
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: "Public",
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+        {
+          cidrMask: 24,
+          name: "Private",
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+        },
+      ],
+    });
+
     const { vpc } = this;
     // s3 bucket
     const bucketName = ssm.StringParameter.valueForStringParameter(
