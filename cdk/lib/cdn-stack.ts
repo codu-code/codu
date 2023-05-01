f9bd94e5d43bd4a606d91ed0c10fc0adbf545793
Niall Maher <nialljoemaher@gmail.com>
2023-05-01 18:38:26 +0100

    Test lambda@edge

--- cdk/lib/cdn-stack.ts
+++ cdk/lib/cdn-stack.ts
@@ -6,6 +6,9 @@ import * as route53 from "aws-cdk-lib/aws-route53";
 import * as acm from "aws-cdk-lib/aws-certificatemanager";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import * as targets from "aws-cdk-lib/aws-route53-targets";
+import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as path from "path";
 
 interface Props extends cdk.StackProps {
   loadbalancer: cdk.aws_elasticloadbalancingv2.ApplicationLoadBalancer;
@@ -30,6 +33,7 @@ export class CdnStack extends cdk.Stack {
     );
 
     const wwwDomainName = `www.${domainName}`;
+    const edgeDomainName = `edge.${domainName}`;
 
     const zone = route53.HostedZone.fromHostedZoneAttributes(this, "MyZone", {
       hostedZoneId,
@@ -153,5 +157,40 @@ export class CdnStack extends cdk.Stack {
 
     new route53.ARecord(this, "WebAliasRecord", recordProps);
     new route53.AaaaRecord(this, "AaaaWebAliasRecord", recordProps);
+
+    const edgeContentBucket = new s3.Bucket(this, "EdgeContentBucket");
+    const myCdnOai = new cloudfront.OriginAccessIdentity(this, "CdnOai");
+    edgeContentBucket.grantRead(myCdnOai);
+
+    const edgeFunction = new lambda.Function(this, "EdgeFunction", {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      handler: "index.handler",
+      code: lambda.Code.fromAsset(path.join(__dirname, "../og")),
+      currentVersionOptions: {
+        removalPolicy: cdk.RemovalPolicy.DESTROY,
+      },
+    });
+
+    const edgeDistribution = new cloudfront.Distribution(
+      this,
+      "ImageDistribution",
+      {
+        defaultBehavior: {
+          origin: new origins.S3Origin(edgeContentBucket),
+          edgeLambdas: [
+            {
+              functionVersion: edgeFunction.currentVersion,
+              eventType: cloudfront.LambdaEdgeEventType.ORIGIN_RESPONSE,
+            },
+          ],
+          viewerProtocolPolicy:
+            cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        },
+      }
+    );
+
+    new cdk.CfnOutput(this, "EdgeDistributionDomainName", {
+      value: edgeDistribution.distributionDomainName,
+    });
   }
 }
