be9380a90278437d7f201b24e95e357a06892a18
John Allen <john.allen.ireland@gmail.com>
2023-05-14 21:11:24 +0100

    feat: added new props to prompt service and updated style to match site

--- components/PromptService/PromptService.tsx
+++ components/PromptService/PromptService.tsx
@@ -1,17 +1,23 @@
 import { useRouter } from "next/router";
 import React, { useState, useEffect, useCallback } from "react";
 import { Modal } from "../Modal/Modal";
-import { ExclamationIcon, XIcon } from "@heroicons/react/outline";
+import { ExclamationIcon, PlusSmIcon, XIcon } from "@heroicons/react/outline";
 import { Dialog } from "@headlessui/react";
 
 export interface serviceProps {
   shouldConfirmLeave: boolean;
   updateParent: (...args: any[]) => any;
+  title: string;
+  subTitle: string;
+  content?: string;
 }
 
 export const PromptDialog = ({
   updateParent,
   shouldConfirmLeave,
+  title,
+  subTitle,
+  content,
 }: serviceProps): React.ReactElement<serviceProps> => {
   const [showPromptDialog, setshowPromptDialog] = useState(false);
   const [nextRouterPath, setNextRouterPath] = useState<string>();
@@ -36,7 +42,7 @@ export const PromptDialog = ({
 
   const cancelRouteChange = () => {
     updateParent("cancel");
-    setNextRouterPath(null);
+    setNextRouterPath(undefined);
     setshowPromptDialog(false);
   };
 
@@ -44,7 +50,7 @@ export const PromptDialog = ({
     updateParent("confirm");
     setshowPromptDialog(false);
     removeListener();
-    router.push(nextRouterPath);
+    if (nextRouterPath) router.push(nextRouterPath);
   };
 
   const removeListener = () => {
@@ -61,7 +67,7 @@ export const PromptDialog = ({
       <div className="hidden sm:block absolute top-0 right-0 pt-4 pr-4">
         <button
           type="button"
-          className="bg-white text-neutral-400 hover:text-neutral-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          className="bg-neutral-900 text-neutral-400 hover:text-neutral-500 focus:outline-none"
           onClick={cancelRouteChange}
         >
           <span className="sr-only">Close</span>
@@ -69,44 +75,42 @@ export const PromptDialog = ({
         </button>
       </div>
       <div className="sm:flex sm:items-start">
-        <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+        <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-gradient-to-r from-orange-400 to-pink-600 sm:mx-0 sm:h-10 sm:w-10">
           <ExclamationIcon
-            className="h-6 w-6 text-red-600"
+            className="h-6 w-6 text-white-600 "
             aria-hidden="true"
           />
         </div>
         <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
           <Dialog.Title
             as="h3"
-            className="text-lg leading-6 font-medium text-black"
+            className="text-lg leading-6 font-medium text-white"
           >
-            Unsaved Changes
+            {title}
           </Dialog.Title>
           <div className="mt-2">
-            <p className="text-sm text-neutral-500">
-              You have unsaved changes.
-            </p>
-            <p className="text-sm text-neutral-500 mt-2">
-              Changes that you have made will be lost.
-            </p>
+            <p className="text-sm text-neutral-500">{subTitle}</p>
+            {content && (
+              <p className="text-sm text-neutral-500 mt-2">{content}</p>
+            )}
           </div>
         </div>
       </div>
       <div className="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
         <button
+          className="ml-4 primary-button"
           type="button"
           disabled={false}
-          className="w-full inline-flex justify-center border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
           onClick={confirmRouteChange}
         >
           Continue without saving
         </button>
         <button
           type="button"
-          className="mt-3 w-full inline-flex justify-center border border-neutral-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-neutral-700 hover:text-neutral-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:w-auto sm:text-sm"
+          className="rounded-md mt-3 w-full inline-flex justify-center border border-neutral-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-neutral-700 hover:text-neutral-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:w-auto sm:text-sm"
           onClick={cancelRouteChange}
         >
-          keep editing
+          Keep editing
         </button>
       </div>
     </Modal>
