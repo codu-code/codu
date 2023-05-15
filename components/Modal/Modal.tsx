90551e1360da8443c1dc25ef9a082f8f7517dace
John <46611809+JohnAllenTech@users.noreply.github.com>
2023-05-15 19:38:18 +0100

    feat: added new props to prompt service and updated style to match site (#282)
    Co-authored-by: John Allen <john.allen.ireland@gmail.com>

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -34,7 +34,7 @@ export function Modal({ open, onClose, children }: Props) {
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative bg-white border-2 border-black px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full sm:p-6">
+              <Dialog.Panel className="relative bg-neutral-900 border-2 border-black px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full sm:p-6">
                 {children}
               </Dialog.Panel>
             </Transition.Child>
--- pages/my-posts/index.tsx
+++ pages/my-posts/index.tsx
@@ -74,7 +74,7 @@ const MyPosts: NextPage = ({
         <div className="hidden sm:block absolute top-0 right-0 pt-4 pr-4">
           <button
             type="button"
-            className="bg-white text-neutral-400 hover:text-neutral-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            className="bg-neutral-900 text-neutral-400 hover:text-neutral-500 focus:outline-none"
             onClick={() => setSelectedArticleToDelete(undefined)}
           >
             <span className="sr-only">Close</span>
@@ -82,16 +82,16 @@ const MyPosts: NextPage = ({
           </button>
         </div>
         <div className="sm:flex sm:items-start">
-          <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+          <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-gradient-to-r from-orange-400 to-pink-600 sm:mx-0 sm:h-10 sm:w-10">
             <ExclamationIcon
-              className="h-6 w-6 text-red-600"
+              className="h-6 w-6 text-white-600"
               aria-hidden="true"
             />
           </div>
           <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
             <Dialog.Title
               as="h3"
-              className="text-lg leading-6 font-medium text-black"
+              className="text-lg leading-6 font-medium text-white"
             >
               Delete article
             </Dialog.Title>
@@ -110,7 +110,7 @@ const MyPosts: NextPage = ({
           <button
             type="button"
             disabled={deleteStatus === "loading"}
-            className="w-full inline-flex justify-center border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
+            className="ml-4 primary-button"
             onClick={() => {
               if (selectedArticleToDelete)
                 mutate({ id: selectedArticleToDelete });
@@ -120,7 +120,7 @@ const MyPosts: NextPage = ({
           </button>
           <button
             type="button"
-            className="mt-3 w-full inline-flex justify-center border border-neutral-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-neutral-700 hover:text-neutral-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:w-auto sm:text-sm"
+            className="mt-3 rounded-md w-full inline-flex justify-center border border-neutral-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-neutral-700 hover:text-neutral-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:w-auto sm:text-sm"
             onClick={() => setSelectedArticleToDelete(undefined)}
           >
             Cancel
