2e05aa1e471f7b32fe6b1030d264cdc5fd98e92a
Daniel Callaghan <dan_cal89@hotmail.com>
2023-05-27 11:27:10 +0000

    add h3 and h4 heading toolbar buttons

--- components/SlateEditor/HooveringToolbar.tsx
+++ components/SlateEditor/HooveringToolbar.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import { FormatBold } from '@styled-icons/material/FormatBold';
-import { FormatItalic } from '@styled-icons/material/FormatItalic';
-import { FormatUnderlined } from '@styled-icons/material/FormatUnderlined';
-import { FormatSize } from '@styled-icons/material';
-import { Link as LinkIcon } from '@styled-icons/material/Link';
-import { Editor } from 'slate';
+import React from "react";
+import { FormatBold } from "@styled-icons/material/FormatBold";
+import { FormatItalic } from "@styled-icons/material/FormatItalic";
+import { FormatUnderlined } from "@styled-icons/material/FormatUnderlined";
+import { FormatSize, FormatQuote } from "@styled-icons/material";
+import { Code } from "@styled-icons/material";
+import { Title } from "@styled-icons/material";
+import { Link as LinkIcon } from "@styled-icons/material/Link";
+import { TextFields } from "@styled-icons/material";
+import { Editor } from "slate";
 import {
   BalloonToolbar,
   BalloonToolbarProps,
@@ -12,19 +15,24 @@ import {
   MARK_BOLD,
   MARK_ITALIC,
   MARK_UNDERLINE,
-  MARK_FONT_SIZE,
-  ELEMENT_LINK,
+  // MARK_FONT_SIZE,
+  // ELEMENT_LINK,
+  ELEMENT_H4,
+  ELEMENT_H3,
   MarkToolbarButton,
   WithPartial,
   usePlateEditorRef,
   LinkToolbarButton,
-  ToolbarButton
+  ToolbarButton,
+  BlockToolbarButton,
+  CodeBlockToolbarButton,
+  ELEMENT_BLOCKQUOTE,
   // UseVirtualFloatingOptions
-} from '@udecode/plate';
-import { applyFontSize } from './customPlugins/createFontSizePlugin';
+} from "@udecode/plate";
+import { applyFontSize } from "./customPlugins/createFontSizePlugin";
 
 const HooveringToolbar = (
-  props: WithPartial<BalloonToolbarProps, 'children'>
+  props: WithPartial<BalloonToolbarProps, "children">
 ) => {
   const { children, ...balloonToolbarProps } = props;
   // console.log('my props ', balloonToolbarProps)
@@ -32,22 +40,20 @@ const HooveringToolbar = (
   const editor = usePlateEditorRef();
 
   const arrow = false;
-  const theme = 'dark';
-
-  
+  const theme = "dark";
 
   // TODO: Define positioning of the toolbar and pass into BallonToolbar as floatingOptions={floatingOptions}
   // const floatingOptions: UseVirtualFloatingOptions = {
   //   getBoundingClientRect: () => ({
   //     width: 0,
   //     height: 0,
-  //     top: 0,    
-  //     right: 0, 
+  //     top: 0,
+  //     right: 0,
   //     bottom: 0,
-  //     left: 0,   
+  //     left: 0,
   //   }),
   // };
-  
+
   return (
     <BalloonToolbar theme={theme} arrow={arrow} {...balloonToolbarProps}>
       <MarkToolbarButton
@@ -70,28 +76,46 @@ const HooveringToolbar = (
         icon={<LinkIcon />}
         actionHandler="onMouseDown"
       />
-      <ToolbarButton
-  icon={<FormatSize />}
-  onMouseDown={(event) => {
-    event.preventDefault();
+      <BlockToolbarButton
+        type={getPluginType(editor, ELEMENT_H3)}
+        icon={<Title />}
+        actionHandler="onMouseDown"
+      />
+      <BlockToolbarButton
+        type={getPluginType(editor, ELEMENT_H4)}
+        icon={<TextFields />}
+        actionHandler="onMouseDown"
+      />
+      {/* <ToolbarButton
+        icon={<FormatSize />}
+        onMouseDown={(event) => {
+          event.preventDefault();
 
-    const nodes = Array.from(
-      Editor.nodes(editor, {
-        at: editor.selection || undefined,
-        match: Text.isText,
-      })
-    );
+          const nodes = Array.from(
+            Editor.nodes(editor, {
+              at: editor.selection || undefined,
+              match: Text.isText,
+            })
+          );
 
-    const isFontSizeIncreased = nodes.some(([node]) => node.fontSize === true);
+          const isFontSizeIncreased = nodes.some(
+            ([node]) => node.fontSize === true
+          );
 
-    // Call applyFontSize with the desired action
-    applyFontSize(editor, !isFontSizeIncreased);
-  }}
-/>
+          // Call applyFontSize with the desired action
+          applyFontSize(editor, !isFontSizeIncreased);
+        }}
+      /> */}
+      {/* <BlockToolbarButton
+        type={getPluginType(editor, ELEMENT_BLOCKQUOTE)}
+        icon={<FormatQuote />}
+        actionHandler="onMouseDown"
+      /> */}
+      {/* <CodeBlockToolbarButton icon={<Code />} actionHandler="onMouseDown"/> */}
 
       {children}
     </BalloonToolbar>
   );
 };
 
-export default HooveringToolbar
+export default HooveringToolbar;
