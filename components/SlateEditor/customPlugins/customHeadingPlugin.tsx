6b78c8f840a57640627dc834f71438e57dd2d8f7
Daniel Callaghan <dan_cal89@hotmail.com>
2023-06-12 13:40:12 +0000

    update heading styles

--- components/SlateEditor/customPlugins/customHeadingPlugin.tsx
+++ components/SlateEditor/customPlugins/customHeadingPlugin.tsx
@@ -4,7 +4,7 @@ export const CustomH3Component = (props: PlateRenderElementProps) => {
   const { attributes, children, element } = props;
 
   return (
-      <h3 className="text-white py-2 text-3xl" {...attributes}>
+      <h3 className="text-white py-2 text-2xl font-bold" {...attributes}>
         {children}
       </h3>
     );
@@ -14,7 +14,7 @@ export const CustomH4Component = (props: PlateRenderElementProps) => {
   const { attributes, children, element } = props;
 
   return (
-      <h4 className="text-white py-2 text-2xl" {...attributes}>
+      <h4 className="text-white py-2 text-xl font-bold" {...attributes}>
         {children}
       </h4>
     );
