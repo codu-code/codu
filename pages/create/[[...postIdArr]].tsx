ef11385dd4376987b129305d090cabcb7a258349
Daniel Callaghan <dan_cal89@hotmail.com>
2023-06-14 22:37:55 +0000

    add prism highlighting to code block

--- pages/create/[[...postIdArr]].tsx
+++ pages/create/[[...postIdArr]].tsx
@@ -21,7 +21,8 @@ import SlateEditor from "../../components/SlateEditor/SlateEditor";
 
 import { htmlToSlate, slateToHtml } from 'slate-serializers'
 import { config as htmlToSlateConfig } from "../../components/SlateEditor/htmlToSlateConfig";
-import parse from 'html-react-parser';
+import parse, { domToReact, htmlToDOM } from 'html-react-parser';
+import Prism from 'prismjs';
 import { config as slateToHTMLConfig } from "../../components/SlateEditor/slateToHTMLConfig";
 
 const Create: NextPage = () => {
@@ -43,6 +44,39 @@ const Create: NextPage = () => {
   const [slateChecked, setSlateChecked] = useState<boolean>(false)
   const allowUpdate = unsavedChanges && !delayDebounce;
   const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+const parseOptions = {
+  replace: ({ attribs, name, children }) => {
+    if (!attribs || name !== 'pre') return;
+
+    const language = attribs.class && attribs.class.replace('language-', '');
+    const codeTags = children && children.filter(child => child.name === 'code');
+
+    if (codeTags && codeTags.length) {
+      const processedCode = codeTags.map(codeTag => {
+        const isCodeEmpty = codeTag.children.length === 0;
+
+        if (isCodeEmpty) {
+          return <code className="block" style={{ minHeight: '1em' }} />;
+
+        } else {
+          const code = codeTag.children.map(child => child.data || '').join('');
+
+          if (language && Prism.languages[language]) {
+            const highlightedCode = Prism.highlight(code, Prism.languages[language], language);
+            return <code className="block" dangerouslySetInnerHTML={{ __html: highlightedCode }} />;
+          } else {
+            return <code className="block">{code}</code>;
+          }
+        }
+      });
+
+      return <pre className={language ? `language-${language}` : ''}>{processedCode}</pre>;
+    }
+  },
+};
+
+
 
 
   const {
@@ -547,7 +581,7 @@ useEffect(() => {
                                 }
                               )} */}
                               <div className="slateP">
-                                {parse(replaceEmptyTags(body))}
+                                {parse(replaceEmptyTags(body), parseOptions)}
                               </div>
 
                             </article>
