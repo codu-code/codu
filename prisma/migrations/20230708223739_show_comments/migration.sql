0e5af28d3221b7f247359b7f33271fb6cd990326
Niall Maher <niall@codu.ie>
2023-07-28 22:35:40 +0100

    remove unecessary migration

--- prisma/migrations/20230708223739_show_comments/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `allowComments` on the `Post` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Post" DROP COLUMN "allowComments",
-ADD COLUMN     "showComments" BOOLEAN NOT NULL DEFAULT true;
