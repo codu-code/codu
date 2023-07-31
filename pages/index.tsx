4de4a34e2154c8506e2bd126727ccd11392c92f6
Niall Maher <niall@codu.ie>
2023-07-31 08:13:43 +0100

    Alter hero layout for mobile exp

--- pages/index.tsx
+++ pages/index.tsx
@@ -92,28 +92,64 @@ const Home: NextPage = () => {
           <div>
             <main className="relative">
               <Atropos
-                rotateXMax={0.2}
-                rotateYMax={0.2}
+                rotateXMax={0.4}
+                rotateYMax={0.4}
                 stretchX={1}
                 stretchY={0.2}
-                stretchZ={0.2}
+                stretchZ={0.3}
                 highlight={false}
-                className="h-[620px] sm:h-[900px] w-full overflow-hidden relative"
+                className="h-[calc(100vh_-_100px)] sm:h-[900px] w-full overflow-hidden relative"
               >
-                <div>
-                  <Image
-                    placeholder="blur"
-                    className="absolute h-full w-full object-cover -z-10"
-                    src={space}
-                    data-atropos-offset="-2"
-                    alt="Realistic space sky which is black with stars scattered across."
-                    onLoad={() => {
-                      setStarsLoaded(true);
-                    }}
-                  />
+                <Image
+                  placeholder="blur"
+                  className="absolute h-full w-full object-cover -z-10"
+                  src={space}
+                  data-atropos-offset="-2"
+                  alt="Realistic space sky which is black with stars scattered across."
+                  onLoad={() => {
+                    setStarsLoaded(true);
+                  }}
+                />
+
+                <div className="absolute -z-10  md:max-h-[800px] md:max-w-[800px] sm:max-h-[800px] sm:max-w-[600px] max-h-[480px] max-w-[480px] left-0 right-0 mx-auto sm:-bottom-60 md:-bottom-96 -bottom-28">
+                  <div className="relative mx-auto brightness-75">
+                    <Image
+                      src={moon}
+                      data-atropos-offset="1"
+                      alt="Photograph of the moon"
+                      sizes="100vw"
+                      style={{
+                        width: "100%",
+                        height: "auto",
+                      }}
+                      onLoad={() => {
+                        setMoonLoaded(true);
+                      }}
+                    />
+                  </div>
+                  <div className="absolute h-[240px] w-[240px] md:h-[350px] md:w-[350px] right-0 md:-right-28 top-10">
+                    <Image
+                      height={350}
+                      width={350}
+                      src={rocketman}
+                      data-atropos-offset="8"
+                      alt="3D claymation style model of a astronaut on a rocket"
+                      sizes="100vw"
+                      style={{
+                        width: "100%",
+                        height: "auto",
+                      }}
+                      onLoad={() => {
+                        setRocketLoaded(true);
+                      }}
+                    />
+                  </div>
                 </div>
 
-                <div data-atropos-offset="0" className="mt-24 md:mt-40">
+                <div
+                  data-atropos-offset="0"
+                  className="h-full flex flex-col justify-center"
+                >
                   <Image
                     width={340}
                     height={200}
@@ -124,7 +160,7 @@ const Home: NextPage = () => {
                     }`}
                   />
                   <h1
-                    className={`text-5xl sm:text-7xl font-extrabold tracking-tight text-center text-white mt-8 duration-500 ${
+                    className={`drop-shadow-2xl text-5xl sm:text-7xl font-extrabold tracking-tight text-center text-white mt-8 duration-500 ${
                       isReady ? "opacity-100" : "opacity-0"
                     }`}
                   >
@@ -156,59 +192,23 @@ const Home: NextPage = () => {
                     </button>
                   </div>
                 </div>
-
-                <div className="-z-10 relative md:h-[800px] md:w-[800px] mx-auto">
-                  <div className="-mt-24 md:-mt-20 mx-auto brightness-75">
-                    <Image
-                      src={moon}
-                      data-atropos-offset="1"
-                      alt="Photograph of the moon"
-                      sizes="100vw"
-                      style={{
-                        width: "100%",
-                        height: "auto",
-                      }}
-                      onLoad={() => {
-                        setMoonLoaded(true);
-                      }}
-                    />
-                  </div>
-                  <div className="h-[280px] w-[280px] md:h-[350px] md:w-[350px] absolute right-0 md:-right-28 top-8">
-                    <Image
-                      height={350}
-                      width={350}
-                      src={rocketman}
-                      data-atropos-offset="8"
-                      alt="3D claymation style model of a astronaut on a rocket"
-                      sizes="100vw"
-                      style={{
-                        width: "100%",
-                        height: "auto",
-                      }}
-                      onLoad={() => {
-                        setRocketLoaded(true);
-                      }}
-                    />
-                  </div>
-                </div>
                 <div
-                  className="absolute bottom-0 w-full h-40 bg-gradient-to-t from-black"
+                  className="absolute bottom-0 w-full h-20 bg-gradient-to-t from-black"
                   data-atropos-offset="-2"
                 />
                 <div
-                  className="absolute top-0 w-full h-40 bg-gradient-to-b from-black"
+                  className="absolute top-0 w-full h-20 bg-gradient-to-b from-black"
                   data-atropos-offset="-2"
                 />
               </Atropos>
             </main>
             <section className="bg-neutral-300 px-2" id="cta">
               <div className="mx-auto lg:max-w-5xl sm:max-w-2xl py-20 sm:py-32">
-                <h2 className="text-2xl md:text-left text-center font-semibold tracking-tight text-gray-900 sm:text-4xl">
+                <h2 className="text-2xl md:text-left text-center font-semibold tracking-tight text-gray-900 sm:text-4xl max-w-[660px]">
                   <span className="font-extrabold">Sign up today</span> to
-                  become a writer and
-                  <br />
-                  get a <span className="font-extrabold">free</span> invite to
-                  our Discord community.
+                  become a writer and get a{" "}
+                  <span className="font-extrabold">free</span> invite to our
+                  Discord community.
                 </h2>
                 <div className="mt-8 flex items-center gap-x-6 justify-center md:justify-start">
                   <Link href="/get-started" className="primary-button">
