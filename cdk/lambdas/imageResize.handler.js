efb7a3182c3b7e592cff6469610dae291874e25d
Niall Maher <niall@codu.ie>
2023-08-17 21:33:25 +0100

    Testing a resizing function (#386)

---
+++ cdk/lambdas/imageResize.handler.js
@@ -0,0 +1,41 @@
+import { S3 } from "@aws-sdk/client-s3";
+import sharp from "sharp";
+
+const s3 = new S3({});
+
+exports.handler = async (event) => {
+  const bucket = event.Records[0].s3.bucket.name;
+  const key = decodeURIComponent(
+    event.Records[0].s3.object.key.replace(/\+/g, " ")
+  );
+
+  const params = {
+    Bucket: bucket,
+    Key: key,
+  };
+
+  try {
+    const image = await s3.getObject(params);
+    const bodyStream = image.Body;
+    if (!bodyStream) throw new Error("BodyStream is empty");
+
+    const resizedImage = await sharp(bodyStream)
+      .resize(200)
+      .webp({ quality: 80 })
+      .toBuffer();
+
+    await s3.putObject({
+      Bucket: bucket,
+      Key: `resized/${key}`,
+      Body: resizedImage,
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Image resized successfully!" }),
+    };
+  } catch (error) {
+    console.error("Error processing image:", error);
+    throw new Error("Error processing image");
+  }
+};
