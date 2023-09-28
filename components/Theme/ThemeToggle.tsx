de33841a1a882cdce6ca11fa3b9f93c42ccf89a6
Darrach Barneveld <darrachbarneveld91@gmail.com>
2023-09-28 20:37:31 +0100

    Fixed css styling alignment on toggle

--- components/Theme/ThemeToggle.tsx
+++ components/Theme/ThemeToggle.tsx
@@ -16,11 +16,11 @@ const ThemeToggle: FunctionComponent<ThemeToggleProps> = () => {
   return (
     <Menu
       as="div"
-      className="relative inline-block text-neutral-400 hover:text-neutral-300 focus:text-neutral-300 focus:outline-none focus:ring-2 focus:ring-rose-500 focus:ring-offset-2 ml-3 rounded-full  "
+      className="relative flex justify-center items-center text-neutral-400 hover:text-neutral-300 focus:text-neutral-300 focus:outline-none focus:ring-2 focus:ring-rose-500 focus:ring-offset-2 ml-3 rounded-full"
     >
-      <Menu.Button className="">
+      <Menu.Button className="relative h-full">
         <SunIcon className="w-6 h-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-        <MoonIcon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <MoonIcon className="absolute top-0 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
         <span className="sr-only">Toggle theme</span>
       </Menu.Button>
       <Transition
@@ -32,7 +32,7 @@ const ThemeToggle: FunctionComponent<ThemeToggleProps> = () => {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="origin-top-right absolute top-7 right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 px-1 ring-black ring-opacity-5 focus:outline-none">
+        <Menu.Items className="origin-top-right top-7 absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 px-1 ring-black ring-opacity-5 focus:outline-none">
           <Menu.Item
             as="button"
             className="relative flex cursor-default text-base md:text-sm select-none items-center rounded-sm font-medium outline-none hover:hover:bg-neutral-200   focus:hover:bg-neutral-200  data-[disabled]:pointer-events-none data-[disabled]:opacity-50 dark:focus:bg-slate-700 text-black w-full px-4 py-2 
@@ -54,7 +54,7 @@ const ThemeToggle: FunctionComponent<ThemeToggleProps> = () => {
           </Menu.Item>
           <Menu.Item
             as="button"
-            className="relative flex cursor-default text-base md:text-sm select-none items-center rounded-sm px-4 py-2  font-medium outline-none w-full hover:hover:bg-neutral-200   focus:hover:bg-neutral-200  data-[disabled]:pointer-events-none data-[disabled]:opacity-50 dark:focus:bg-slate-700 text-black
+            className="relative flex cursor-default text-base md:text-sm select-none items-center rounded-sm px-4 py-2 font-medium outline-none w-full hover:hover:bg-neutral-200   focus:hover:bg-neutral-200  data-[disabled]:pointer-events-none data-[disabled]:opacity-50 dark:focus:bg-slate-700 text-black
 "
             onClick={() => setTheme("dark")}
           >
