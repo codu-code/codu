114d652a49417117b5d6b6d95c957fa4016e6574
Niall Maher <niall@codu.ie>
2023-09-30 16:45:39 +0100

    Feat/new editor starter (#424)
    * WIP new editor starter from template
    * Fix package-lock
    * Remove BlobResult

---
+++ pages/alpha/new/index.tsx
@@ -0,0 +1,9 @@
+import Editor from "../../../components/editor/editor";
+
+export default function Page() {
+  return (
+    <div className="flex min-h-screen flex-col items-center sm:px-5 sm:pt-[calc(20vh)]">
+      <Editor />
+    </div>
+  );
+}
