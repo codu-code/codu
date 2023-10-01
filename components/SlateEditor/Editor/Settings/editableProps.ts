0103d965ec98a11863cae606a3fb5a047a5a2ab9
Daniel Callaghan <84312595+xiaoniuniu89@users.noreply.github.com>
2023-10-01 09:55:37 +0100

    refactor(tip tap editor) and kill Slate
    refactor(tip tap editor): 
    * add support for editor integration 
    * remove slate editor and dependencies 
    * add support for rendering content from tip tap editor (#446)

--- components/SlateEditor/Editor/Settings/editableProps.ts
+++
@@ -1,9 +0,0 @@
-import { TEditableProps } from '@udecode/plate';
-import { MyValue } from './plateTypes';
-
-export const editableProps: TEditableProps<MyValue> = {
-  spellCheck: false,
-  autoFocus: false,
-  placeholder: 'Enter your content here 💖',
-  className: "text-xl leading-5 bg-neutral-900 focus:bg-black mt-4 focus:ring-2 focus:ring-gray-300 py-3 px-2 min-h-[40rem]",
-};
--- components/editor/editor/index.tsx
+++ components/editor/editor/index.tsx
@@ -1,65 +1,40 @@
-// "use client";
+"use client";
 
-import { useEffect, useState } from "react";
 import { useEditor, EditorContent } from "@tiptap/react";
 import { TiptapEditorProps } from "./props";
 import { TiptapExtensions } from "./extensions";
-import useLocalStorage from "@/hooks/useLocalStorage";
-import { useDebouncedCallback } from "use-debounce";
-
-import DEFAULT_EDITOR_CONTENT from "./default-content";
 import { EditorBubbleMenu } from "./components/bubble-menu";
 import { ImageResizer } from "@/components/editor/editor/components/image-resizer";
 
-export default function Editor() {
-  const [content, setContent] = useLocalStorage(
-    "content",
-    DEFAULT_EDITOR_CONTENT
-  );
-  const [saveStatus, setSaveStatus] = useState("Saved");
+interface EditorProps {
+  initialValue: string;
+  onChange: (value: string) => void;
+}
 
-  const [hydrated, setHydrated] = useState(false);
-
-  const debouncedUpdates = useDebouncedCallback(async ({ editor }) => {
-    const json = editor.getJSON();
-    setSaveStatus("Saving...");
-    setContent(json);
-    // Simulate a delay in saving.
-    setTimeout(() => {
-      setSaveStatus("Saved");
-    }, 500);
-  }, 750);
+export default function Editor({onChange, initialValue}: EditorProps) {
 
   const editor = useEditor({
     extensions: TiptapExtensions,
     editorProps: TiptapEditorProps,
+    content: JSON.parse(initialValue),
     onUpdate: (e) => {
-      debouncedUpdates(e);
+      const {editor} = e
+      const json = editor.getJSON();
+      console.log(json)
+      onChange(JSON.stringify(json))
     },
     autofocus: "end",
   });
-
-  // Hydrate the editor with the content from localStorage.
-  useEffect(() => {
-    if (editor && content && !hydrated) {
-      editor.commands.setContent(content);
-      setHydrated(true);
-    }
-  }, [editor, content, hydrated]);
 
   return (
     <div
       onClick={() => {
         editor?.chain().focus().run();
       }}
-      className="relative min-h-[500px] w-full max-w-screen-lg border-stone-200 bg-white p-12 px-8 sm:mb-[calc(20vh)] sm:rounded-lg sm:border sm:px-12 sm:shadow-lg text-black placeholder:text-slate-500"
     >
-      <div className="absolute right-5 top-5 mb-5 rounded-lg bg-stone-100 px-2 py-1 text-sm text-stone-400">
-        {saveStatus}
-      </div>
       {editor && <EditorBubbleMenu editor={editor} />}
       {editor?.isActive("image") && <ImageResizer editor={editor} />}
-      <EditorContent editor={editor} />
+      <EditorContent editor={editor}/>
     </div>
   );
 }
