60d4a1de884df54638a3087caae7f4a5557c63e6
DarrachBarneveld <130994206+DarrachBarneveld@users.noreply.github.com>
2023-10-01 09:36:48 +0100

    Theme (#442)
    * Install next-theme, added basic theme toggler ui
    * Created basic theme provider
    * Updated tailwind config with anims and darkmode
    * Added tailwind animations package
    * Added @ ahead of tailwind animate plugin

---
+++ components/Theme/ThemeProvider.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react";
+import { ThemeProvider } from "next-themes";
+
+const Providers = ({ children }: { children: ReactNode }) => {
+  return (
+    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+      {children}
+    </ThemeProvider>
+  );
+};
+
+export default Providers;
