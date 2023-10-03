35d2e114708fc12694be3c295a5b5b5c6586c2d9
Darrach Barneveld <darrachbarneveld91@gmail.com>
2023-10-03 09:28:25 +0100

    Removed commented code, added sr span to button instead of icon

--- components/Nav/AnimatedHamburger.tsx
+++ components/Nav/AnimatedHamburger.tsx
@@ -9,7 +9,6 @@ const AnimatedHamburger: FunctionComponent<AnimatedHamburgerProps> = ({
 }) => {
   return (
     <div className="text-neutral-400 w-6 h-6 relative focus:outline-none hover:text-white focus:text-white">
-      <span className="sr-only">Open main menu</span>
       <div className="block w-5 absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
         <span
           aria-hidden="true"
@@ -19,13 +18,13 @@ const AnimatedHamburger: FunctionComponent<AnimatedHamburgerProps> = ({
         ></span>
         <span
           aria-hidden="true"
-          className={`block absolute  h-0.5 w-5 bg-current   transform transition ease-in-out ${
+          className={`block absolute h-0.5 w-5 bg-current   transform transition ease-in-out ${
             open && "opacity-0"
           }`}
         ></span>
         <span
           aria-hidden="true"
-          className={`block absolute  h-0.5 w-5 bg-current transform  transition ease-in-out ${
+          className={`block absolute h-0.5 w-5 bg-current transform  transition ease-in-out ${
             open && "-rotate-45"
           } ${!open && "translate-y-1.5"}`}
         ></span>
--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -4,18 +4,13 @@ import Link from "next/link";
 import { Fragment } from "react";
 import { useSession } from "next-auth/react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
-import { MenuIcon, XIcon } from "@heroicons/react/outline";
 import { PlusSmIcon, BellIcon } from "@heroicons/react/solid";
-import { navigation, subNav, userSubNav } from "../../config/site_settings";
+import { navigation } from "../../config/site_settings";
 import { trpc } from "../../utils/trpc";
 import ThemeToggle from "../Theme/ThemeToggle";
 import MobileNav from "./MobileNav";
 import ThemeToggleMobile from "../Theme/ThemeToggleMobile";
 import AnimatedHamburger from "./AnimatedHamburger";
-
-function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(" ");
-}
 
 const Nav = () => {
   const { data: session } = useSession();
@@ -217,132 +212,13 @@ const Nav = () => {
                 </Link>
                 {/* Mobile menu button */}
                 <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-neutral-400 hover:text-white hover:bg-neutral-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  {/* <span className="sr-only">Open main menu</span>
-                  {open ? (
-                    <XIcon className="block h-6 w-6" aria-hidden="true" />
-                  ) : (
-                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
-                  )} */}
+                  <span className="sr-only">Open main menu</span>
                   <AnimatedHamburger open={open} />
                 </Disclosure.Button>
               </div>
             </div>
           </div>
 
-          {/* <Disclosure.Panel className="md:hidden relative z-10 border-b-2 border-white">
-            <div className="px-2 pt-2 pb-3 space-y-1">
-              {navigation.map((item) =>
-                item.href.includes("http") ? (
-                  <Disclosure.Button
-                    as="a"
-                    key={item.name}
-                    href={item.href}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-neutral-300 hover:bg-neutral-900 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-                  >
-                    {item.name}
-                  </Disclosure.Button>
-                ) : (
-                  <Link key={item.name} href={item.href}>
-                    <Disclosure.Button
-                      as="div"
-                      className="text-neutral-300 hover:bg-neutral-900 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-                    >
-                      {item.name}
-                    </Disclosure.Button>
-                  </Link>
-                )
-              )}
-
-              <div className="pt-3 pb-3 border-t border-neutral-700 flex  flex-col space-y-1">
-                {session ? (
-                  <>
-                    {userSubNav.map((item) => (
-                      <Link
-                        className={classNames(
-                          item.fancy
-                            ? "bg-gradient-to-r from-orange-400 to-pink-600 shadow-sm px-4 block justify-center text-white hover:from-orange-300 hover:to-pink-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-300"
-                            : "text-neutral-300 hover:bg-neutral-900 hover:text-white block px-3",
-                          "rounded-md text-base font-medium py-2 text-center"
-                        )}
-                        key={item.name}
-                        href={item.href}
-                      >
-                        {item.name}
-                      </Link>
-                    ))}
-                  </>
-                ) : (
-                  <>
-                    {subNav.map((item) => (
-                      <button
-                        className={classNames(
-                          item.fancy
-                            ? "bg-gradient-to-r from-orange-400 to-pink-600 shadow-sm px-4 block justify-center text-white hover:from-orange-300 hover:to-pink-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-300"
-                            : "text-neutral-300 hover:bg-neutral-900 hover:text-white block px-3",
-                          "rounded-md text-base font-medium py-2"
-                        )}
-                        key={item.name}
-                        onClick={() => signIn()}
-                      >
-                        {item.name}
-                      </button>
-                    ))}
-                  </>
-                )}
-              </div>
-            </div>
-
-            {session && (
-              <div className="px-2 pt-2 space-y-1">
-                <div className="pt-4 pb-3 border-t border-neutral-700">
-                  <div className="flex items-center px-5">
-                    <div className="flex-shrink-0">
-                      <img
-                        className="h-10 w-10 rounded-full"
-                        src={session.user?.image || "/images/person.png"}
-                        alt={`${session.user?.name}'s avatar`}
-                      />
-                    </div>
-                    <div className="ml-3">
-                      <div className="text-base font-medium text-white">
-                        {session.user?.name}
-                      </div>
-                      <div className="text-sm font-medium text-neutral-400">
-                        {session.user?.email}
-                      </div>
-                    </div>
-                  </div>
-                  <div className="mt-3 space-y-1">
-                    {userNavigation.map((item) =>
-                      item.onClick ? (
-                        <Disclosure.Button
-                          key={item.name}
-                          as="button"
-                          onClick={item.onClick}
-                          className="cursor-pointer block w-full text-left px-3 py-2 rounded-md text-base font-medium text-neutral-400 hover:text-white hover:bg-neutral-900"
-                        >
-                          {item.name}
-                        </Disclosure.Button>
-                      ) : (
-                        <Link key={item.name} href={item.href}>
-                          <Disclosure.Button
-                            as="div"
-                            className="cursor-pointer block px-3 py-2 rounded-md text-base font-medium text-neutral-400 hover:text-white hover:bg-neutral-900"
-                          >
-                            {item.name}
-                          </Disclosure.Button>
-                        </Link>
-                      )
-                    )}
-                  </div>
-                </div>
-              </div>
-            )}
-          </Disclosure.Panel> */}
-
-          {/* Testing MobileNav */}
           <MobileNav
             session={session}
             count={count}
