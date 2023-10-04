44d79a5ce42a77a3164061b223cbe39b3c173e40
DarrachBarneveld <130994206+DarrachBarneveld@users.noreply.github.com>
2023-10-04 19:03:06 +0100

    Added custom hover and focus classes for social logos (#483)
    * Added custom hover and focus classes for social logos
    
    * Added global variable css in root and tailwind config
    
    * Updated tailwing config with background image

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,12 @@ module.exports = {
     extend: {
       colors: {
         black: "#040404",
+        twitter: "var(--twitter-clr)",
+        github: "var(--github-clr)",
+      },
+      backgroundImage: {
+        discord: "var(--discord-clr)",
+        youtube: "var(--youtube-clr)",
       },
     },
   },
