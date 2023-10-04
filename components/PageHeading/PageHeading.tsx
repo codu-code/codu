7ffed6db243459ba861558172d95716b37c5c303
Anurag Yadav <78202060+AnuMessi10@users.noreply.github.com>
2023-10-04 15:29:48 +0530

    Feat: Add Light Theme To Notifications Page (#480)

--- components/PageHeading/PageHeading.tsx
+++ components/PageHeading/PageHeading.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const PageHeading = ({ children }: { children: React.ReactNode }) => (
-  <h1 className="text-3xl tracking-tight font-extrabold text-neutral-50 sm:text-4xl mt-8 border-b pb-4">
+  <h1 className="text-3xl tracking-tight font-extrabold text-neutral-900 dark:text-neutral-50 sm:text-4xl mt-8 border-b pb-4">
     {children}
   </h1>
 );
