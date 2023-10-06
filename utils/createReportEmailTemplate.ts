33f9601f509051303fad25d992fdb8a40a9eef96
john Paul Larkin <johnplarkin@gmail.com>
2023-10-06 12:27:42 +0100

    fix readability issues

--- utils/createReportEmailTemplate.ts
+++ utils/createReportEmailTemplate.ts
@@ -1,7 +1,7 @@
 interface ReportDetails {
-    reportedById: string | undefined;
-    reportedByEmail: string | null | undefined;
-    reportedByUser: string | null | undefined;
+    reportedById?: string;
+    reportedByEmail?: string | null;
+    reportedByUser?: string | null;
     reportedOnName: string;
     reportedOnEmail: string | null;
     reportedComment: string;
@@ -23,22 +23,22 @@ export const createReportEmailTemplate = (reportDetails:ReportDetails) =>
   <div style="font-family: Arial, sans-serif;">
     <div style='padding:3rem; background-color:black; color:white;'>
         <p>Details of reported comment:</p>
-        <p>link to post : <a style="color:yellow" href=${reportDetails.postLink ? reportDetails.postLink : ''}>${reportDetails.postLink ? reportDetails.postLink : ''}</a></p>
-        <p style='padding:3rem; border: 2px solid white;'>comment : ${reportDetails.reportedComment ? reportDetails.reportedComment : ''}</p>
-        <p>commment made by user : ${reportDetails.reportedOnName ? reportDetails.reportedOnName : ''} </p>
-        <p>comment ID : ${reportDetails.commentId ? reportDetails.commentId : ''}</p>
-        <p>users email : ${reportDetails.reportedOnEmail ? reportDetails.reportedOnEmail : ''} </p>   
-        <p>comment report timestamp : ${reportDetails.timeReportSent ? reportDetails.timeReportSent : ''} </p>
+        <p>link to post : <a style="color:yellow" href=${reportDetails.postLink || ''}>${reportDetails.postLink || ''}</a></p>
+        <p style='padding:3rem; border: 2px solid white;'>comment : ${reportDetails.reportedComment || ''}</p>
+        <p>commment made by user : ${reportDetails.reportedOnName || ''} </p>
+        <p>comment ID : ${reportDetails.commentId || ''}</p>
+        <p>users email : ${reportDetails.reportedOnEmail || ''} </p>   
+        <p>comment report timestamp : ${reportDetails.timeReportSent || ''} </p>
     </div>
 
    <br>
    <br>
    <br>
    <br>
-   <p>Comment left the reporter : ${reportDetails.commentMadeByReporter ? reportDetails.commentMadeByReporter : ''} </p>
-   <p>reported by user : ${reportDetails.reportedByUser ? reportDetails.reportedByUser : '' }</p>
-   <p>users email : ${reportDetails.reportedByEmail ? reportDetails.reportedByEmail : ''}</p>
-   <p>users id : ${reportDetails.reportedById ? reportDetails.reportedById : ''}</p>
+   <p>Comment left the reporter : ${reportDetails.commentMadeByReporter || ''} </p>
+   <p>reported by user : ${reportDetails.reportedByUser || '' }</p>
+   <p>users email : ${reportDetails.reportedByEmail || ''}</p>
+   <p>users id : ${reportDetails.reportedById || ''}</p>
   </div>
 
 </body>
