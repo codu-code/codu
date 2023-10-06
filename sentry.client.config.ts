3316d3020e45d13ca56d3a0f3805b37bf417b920
Niall Maher <niall@codu.ie>
2023-10-06 23:02:52 +0100

    Expose sentry environments  (#494)
    * Expose sentry envs
    
    * Add .prettierrc
    
    * Update Sentry config

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -4,24 +4,19 @@
 
 import * as Sentry from "@sentry/nextjs";
 
-const SENTRY_DSN = process.env.NEXT_PUBLIC_SENTRY_DSN;
+const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+const SENTRY_ENVIRONMENT = process.env.SENTRY_ENVIRONMENT;
 
-if (SENTRY_DSN) {
+if (SENTRY_DSN && SENTRY_ENVIRONMENT) {
   Sentry.init({
     dsn: SENTRY_DSN,
-
+    environment: SENTRY_ENVIRONMENT,
     // Adjust this value in production, or use tracesSampler for greater control
     tracesSampleRate: 1,
-
-    // Setting this option to true will print useful information to the console while you're setting up Sentry.
-    debug: false,
-
     replaysOnErrorSampleRate: 1.0,
-
     // This sets the sample rate to be 10%. You may want this to be 100% while
     // in development and sample at a lower rate in production
     replaysSessionSampleRate: 0.1,
-
     // You can remove this option if you're not planning to use the Sentry Session Replay feature:
     integrations: [
       new Sentry.Replay({
