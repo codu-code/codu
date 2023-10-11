e33089bf66c3c502b4cb68dba72ccad0df5c879e
DarrachBarneveld <130994206+DarrachBarneveld@users.noreply.github.com>
2023-10-11 07:23:27 +0100

    Table inception (#510)
    * Added tooltip and toolbar item refactor
    
    * Further refactor of toobaritems
    
    * Added table icon and active class
    
    * Added basic table functionality
    
    * Added custom table row
    
    * Added basic table and styling buttons
    
    * Added tooltip styling

---
+++ components/editor/editor/components/Table/CustomTableNodeView.tsx
@@ -0,0 +1,95 @@
+import { NodeViewContent, NodeViewProps, NodeViewWrapper } from "@tiptap/react";
+import {
+  BookmarkMinus,
+  ColumnsIcon,
+  DeleteIcon,
+  RowsIcon,
+  Trash2Icon,
+} from "lucide-react";
+import { FunctionComponent, ReactNode } from "react";
+
+import styles from "../Toolbar/Toolbar.module.css";
+
+interface TableButtonProps {
+  disabled: boolean;
+  onClick: () => boolean | void;
+  icon: ReactNode;
+  className: string;
+  title: string;
+}
+
+const TableButton: FunctionComponent<TableButtonProps> = ({
+  onClick,
+  disabled,
+  icon,
+  className,
+  title,
+}) => {
+  return (
+    <div className={styles.buttonContainer} data-tooltip={title}>
+      <button
+        onClick={onClick}
+        disabled={disabled}
+        className={`relative flex w-fit h-fit text-neutral-100 cursor-pointer justify-center items-center p-1 rounded-md ${className}`}
+      >
+        {icon}
+        <div className={`${styles.tooltip} top-8`}>
+          <span className="text-sm">{title}</span>
+        </div>
+      </button>
+    </div>
+  );
+};
+
+const CustomTableNodeView = (props: NodeViewProps) => {
+  const { editor } = props;
+
+  return (
+    <NodeViewWrapper>
+      <div className="flex gap-1">
+        <TableButton
+          onClick={() => editor.chain().focus().addColumnAfter().run()}
+          disabled={!editor.can().addColumnAfter()}
+          className="bg-neutral-900 hover:bg-neutral-600"
+          icon={<ColumnsIcon />}
+          title="Add Column"
+        />
+        <TableButton
+          onClick={() => editor.chain().focus().addRowAfter().run()}
+          disabled={!editor.can().addColumnAfter()}
+          className="bg-neutral-900 hover:bg-neutral-600"
+          title="Add Row"
+          icon={<RowsIcon />}
+        />
+        <TableButton
+          onClick={() => editor.chain().focus().deleteColumn().run()}
+          disabled={!editor.can().deleteColumn()}
+          className="bg-red-700 hover:bg-red-400"
+          title="Delete Column"
+          icon={<BookmarkMinus />}
+        />
+
+        <TableButton
+          onClick={() => editor.chain().focus().deleteRow().run()}
+          disabled={!editor.can().deleteRow()}
+          className="bg-red-700 hover:bg-red-400"
+          title="Delete Row"
+          icon={<DeleteIcon />}
+        />
+        <TableButton
+          onClick={() => editor.chain().focus().deleteTable().run()}
+          disabled={!editor.can().deleteTable()}
+          className="bg-red-700 hover:bg-red-400"
+          title="Delete Table"
+          icon={<Trash2Icon />}
+        />
+      </div>
+      <NodeViewContent
+        className="bg-neutral-100 w-full table-fixed overflow-scroll"
+        as="table"
+      ></NodeViewContent>
+    </NodeViewWrapper>
+  );
+};
+
+export default CustomTableNodeView;
