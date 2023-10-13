92c2d276fe6526bac95580bf2a1a2018da7e916b
Niall Maher <niall@codu.ie>
2023-10-13 13:23:47 +0100

    Remove minimize from build (#522)
    * downgrade next
    * Fix trpc.ts file and update envs for app

--- utils/trpc.ts
+++ utils/trpc.ts
@@ -3,11 +3,12 @@ import { createTRPCNext } from "@trpc/next";
 import { type inferRouterInputs, type inferRouterOutputs } from "@trpc/server";
 import superjson from "superjson";
 
-import { type AppRouter } from "../server/trpc/router";
+import { type AppRouter } from "@/server/api/router";
 
 const getBaseUrl = () => {
   if (typeof window !== "undefined") return ""; // browser should use relative url
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+  if (process.env.DOMAIN_NAME) return `https://${process.env.DOMAIN_NAME}`; // SSR should use Domain name or vercel url on Vercel
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
 };
 
