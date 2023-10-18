88e70ec6c22db036f55bd73ce27d336ffc16677b
John Paul Larkin <johnplarkin@gmail.com>
2023-10-18 16:06:11 +0100

    update modal to use headlessUI Dialog component

--- components/Comments/ReportComments.tsx
+++ components/Comments/ReportComments.tsx
@@ -6,6 +6,7 @@ import { signIn, useSession } from "next-auth/react";
 import { createCommentReportEmailTemplate } from "../../utils/createCommentReportEmailTemplate";
 import { api } from "@/server/trpc/react";
 import { createArticleReportEmailTemplate } from "@/utils/createArticleReportEmailTemplate";
+import { Dialog } from "@headlessui/react";
 
 interface Props {
   postTitle?: string;
@@ -43,16 +44,9 @@ export const ReportComments = (props: Props) => {
     postUrl,
     postUsername,
   } = props;
-  const [isModalOpen, setModalOpen] = useState<boolean>(false);
-  const [comment, setComment] = useState("");
 
-  const handleOpenModal = () => setModalOpen(true);
-  const handleCloseModal = () => setModalOpen(false);
-  const handleKeyDown = (event: React.KeyboardEvent<HTMLDialogElement>) => {
-    if (event.key === "Escape") {
-      handleCloseModal();
-    }
-  };
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [comment, setComment] = useState("");
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -107,40 +101,16 @@ export const ReportComments = (props: Props) => {
       sendEmail(mailInputs);
     }
 
-    handleCloseModal();
+    setIsModalOpen(false);
     setComment("");
   };
-
-  const modalRef = useRef<HTMLDialogElement | null>(null);
-
-  useEffect(() => {
-    const modalElement = modalRef.current;
-    if (modalElement) {
-      if (isModalOpen) {
-        modalElement.showModal();
-      } else {
-        modalElement.close();
-      }
-    }
-
-    // Close modal if clicked outside
-    const checkIfClickedOutside = (e: MouseEvent) => {
-      const targetNode = e.target as Node | null;
-      if (isModalOpen && modalRef.current && targetNode?.nodeName === "DIALOG")
-        setModalOpen(false);
-    };
-    document.addEventListener("mousedown", checkIfClickedOutside);
-    return () => {
-      document.removeEventListener("mousedown", checkIfClickedOutside);
-    };
-  }, [isModalOpen]);
 
   return (
     <>
       {!postTitle && (
         <button
           aria-label="flag comment"
-          onClick={() => (session ? handleOpenModal() : signIn())}
+          onClick={() => (session ? setIsModalOpen(true) : signIn())}
           className="mr-4 flex p-1.5 rounded-full hover:bg-neutral-800"
         >
           <Flag className="h-5 " />
@@ -149,72 +119,72 @@ export const ReportComments = (props: Props) => {
 
       {postTitle && (
         <button
-          onClick={() => (session ? handleOpenModal() : signIn())}
+          onClick={() => (session ? setIsModalOpen(true) : signIn())}
           className="p-1 rounded w-full flex px-4 py-2 text-neutral-900 dark:text-neutral-700 hover:bg-neutral-200"
         >
           Report Article
         </button>
       )}
 
-      {isModalOpen && (
-        <dialog
-          aria-modal="true"
-          ref={modalRef}
-          onKeyDown={handleKeyDown}
-          className="p-0 border bg-neutral-900 text-neutral-400 max-w-lg rounded-lg backdrop:bg-gray-700/90"
-        >
-          <div className="p-2 w-full h-full">
-            <h1 className="text-2xl tracking-tight font-bold text-neutral-50 m-8 ml-4">
-              Submit a report
-            </h1>
-            <p className="border p-4 m-4 bg-neutral-700 text-zinc-200">
-              <span>{postTitle ? "Article : " : "Comment : "}</span>
-              {body}
-              {postTitle}
-            </p>
-            <div className="m-4">
-              <p className="mb-2">
-                Is {postTitle ? "something in this article" : "this comment"}{" "}
-                inappropriate?
-              </p>
-              <p>
+      <Dialog open={isModalOpen} onClose={() => setIsModalOpen(false)}>
+        <div className="fixed inset-0 flex w-screen items-center justify-center bg-gray-700/90">
+          <Dialog.Panel className="p-0 border bg-neutral-900 text-neutral-400 max-w-lg rounded-lg relative ">
+            <div className="m-8">
+              <Dialog.Title className="text-2xl tracking-tight font-bold text-neutral-50 text-center">
+                Submit a report
+              </Dialog.Title>
+
+              <Dialog.Description as="div">
+                <p className="m-4 ml-0">
+                  Is {postTitle ? "something in this article" : "this comment"}{" "}
+                  inappropriate?
+                </p>
+                <p className="border p-4  bg-neutral-700 text-zinc-200 rounded">
+                  <span>{postTitle ? "Article : " : "Comment : "}</span>
+                  {body}
+                  {postTitle}
+                </p>
+              </Dialog.Description>
+
+              <p className="m-4 ml-0">
                 Thank you for bringing it to our attention. We take reports very
                 seriously and will thoroughly investigate the matter.
               </p>
+
+              <form>
+                <label htmlFor="report-comment">Leave a comment</label>
+                <textarea
+                  maxLength={300}
+                  id="report-comment"
+                  rows={3}
+                  placeholder="type...."
+                  onChange={(e) => setComment(e.target.value)}
+                  value={comment}
+                  className="rounded"
+                />
+
+                <div className="flex justify-end text-sm mt-8">
+                  <button
+                    className="primary-button"
+                    type="submit"
+                    onClick={handleSubmit}
+                  >
+                    SUBMIT REPORT
+                  </button>
+                </div>
+              </form>
             </div>
 
-            <form className="m-4">
-              <label htmlFor="report-comment">Comment</label>
-              <textarea
-                maxLength={300}
-                id="report-comment"
-                rows={3}
-                placeholder="leave a comment"
-                onChange={(e) => setComment(e.target.value)}
-                value={comment}
-              />
-
-              <div className="flex justify-end mt-8 m-4 text-sm">
-                <button
-                  className="primary-button"
-                  type="submit"
-                  onClick={handleSubmit}
-                >
-                  SUBMIT REPORT
-                </button>
-              </div>
-            </form>
-
             <button
-              onClick={handleCloseModal}
-              aria-label="Close"
+              onClick={() => setIsModalOpen(false)}
+              aria-label="Close modal"
               className="absolute top-6 right-6 p-1 hover:bg-neutral-800 rounded-full"
             >
               <XIcon className="w-8" />
             </button>
-          </div>
-        </dialog>
-      )}
+          </Dialog.Panel>
+        </div>
+      </Dialog>
     </>
   );
 };
