29992a835f9087603eb7cf483d71471cc2deba94
John Paul Larkin <johnplarkin@gmail.com>
2023-10-27 11:02:24 +0100

    add forward to HOC Modal

--- components/ImageDetailsModal/ImageDetailsModal.tsx
+++ components/ImageDetailsModal/ImageDetailsModal.tsx
@@ -1,29 +1,18 @@
+import { Dispatch, SetStateAction, useRef } from "react";
 import { Editor } from "@tiptap/core";
-import {
-  BaseSyntheticEvent,
-  ChangeEvent,
-  Dispatch,
-  FormEvent,
-  SetStateAction,
-  useCallback,
-  useState,
-} from "react";
 import { Modal } from "@/components/Modal/Modal";
 import { XIcon } from "lucide-react";
-import { FieldValues, useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 import { Dialog } from "@headlessui/react";
 
 const imageDetailsSchema = z.object({
   src: z
     .string()
     .trim()
-    .url()
-    .refine((value) => {
-      const urlPattern = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i;
-      return urlPattern.test(value);
-    }),
+    .min(1, { message: "A URL is required." })
+    .url({ message: "Must be a valid URL" }),
   alt: z
     .string()
     .refine((value) => value !== "", { message: "An alt value is required" }),
@@ -52,18 +41,7 @@ export default function ImageDetailsModal(props: Props) {
     resolver: zodResolver(imageDetailsSchema),
   });
 
-  type Submit = {
-    data: ImageDetailsSchema;
-    event: BaseSyntheticEvent<object, any, any> | undefined;
-  };
-
-  const onSubmit = ({ data, event }: Submit) => {
-    // event?.stopPropagation();
-
-    console.log(data.src);
-    console.log(data.alt);
-    console.log(data.title);
-
+  const onSubmit = (data: ImageDetailsSchema) => {
     editor
       ?.chain()
       .focus()
@@ -74,6 +52,8 @@ export default function ImageDetailsModal(props: Props) {
     setIsImageDetailsModalOpen(false);
   };
 
+  const urlInputRef = useRef(null);
+
   return (
     <Modal
       open={isImageDetailsModalOpen}
@@ -81,6 +61,7 @@ export default function ImageDetailsModal(props: Props) {
         reset();
         setIsImageDetailsModalOpen(false);
       }}
+      initialFocusRef={urlInputRef}
     >
       <>
         <Dialog.Title className="text-lg text-center mb-4">
@@ -95,7 +76,10 @@ export default function ImageDetailsModal(props: Props) {
           <button
             type="button"
             className="bg-neutral-900 text-neutral-400 hover:text-neutral-500 focus:outline-none"
-            onClick={() => setIsImageDetailsModalOpen(false)}
+            onClick={() => {
+              setIsImageDetailsModalOpen(false);
+              reset();
+            }}
           >
             <span className="sr-only">Close</span>
             <XIcon className="h-6 w-6" aria-hidden="true" />
@@ -103,7 +87,14 @@ export default function ImageDetailsModal(props: Props) {
         </div>
 
         <form
+        // onSubmit={handleSubmit(onSubmit)}
+
         // onSubmit={handleSubmit((data, event) => onSubmit({ data, event }))}
+
+        // onSubmit={e=>{
+        //   e.stopPropagation();
+        //   handleSubmit((data) => onSubmit( data ))}
+        // }
         >
           <div>
             <label htmlFor="src">Image URL:</label>
@@ -113,8 +104,13 @@ export default function ImageDetailsModal(props: Props) {
               id="src"
               name="src"
               placeholder="Enter image URL..."
+              ref={urlInputRef}
             />
-            {errors && <p>{errors.src?.message}</p>}
+            {errors && (
+              <p className="text-right text-red-600 mt-2">
+                {errors.src?.message}
+              </p>
+            )}
           </div>
 
           <div>
@@ -126,7 +122,11 @@ export default function ImageDetailsModal(props: Props) {
               name="alt"
               placeholder="Enter an alt description..."
             />
-            {errors && <p>{errors.alt?.message}</p>}
+            {errors && (
+              <p className="text-right text-red-600 mt-2">
+                {errors.alt?.message}
+              </p>
+            )}
           </div>
 
           <div>
@@ -138,14 +138,19 @@ export default function ImageDetailsModal(props: Props) {
               name="title"
               placeholder="Enter a title..."
             />
-            {errors && <p>{errors.title?.message}</p>}
+            {errors && (
+              <p className="text-right text-red-600 mt-2">
+                {errors.title?.message}
+              </p>
+            )}
           </div>
 
           <div className="w-full flex justify-end mt-4">
             <button
               type="submit"
+              disabled={isSubmitting}
               className="disabled:opacity-50 rounded ml-5 bg-gradient-to-r from-orange-400 to-pink-600 shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:from-orange-300 hover:to-pink-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-300"
-              onClick={handleSubmit((data, event) => onSubmit({ data, event }))}
+              onClick={handleSubmit(onSubmit)}
             >
               Submit
             </button>
--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -1,46 +1,56 @@
-import React, { Fragment } from "react";
+import React, { Fragment, MutableRefObject, forwardRef } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 
 interface Props {
   open: boolean;
   onClose: () => void;
   children: React.ReactNode;
+  initialFocusRef?: MutableRefObject<HTMLInputElement | null>;
 }
 
-export function Modal({ open, onClose, children }: Props) {
-  return (
-    <Transition.Root show={open} as={Fragment}>
-      <Dialog as="div" className="relative z-10" onClose={onClose}>
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
+const Modal = forwardRef(
+  ({ open, onClose, children, initialFocusRef }: Props) => {
+    return (
+      <Transition.Root show={open} as={Fragment}>
+        <Dialog
+          as="div"
+          className="relative z-10"
+          onClose={onClose}
+          initialFocus={initialFocusRef}
         >
-          <div className="fixed inset-0 bg-neutral-800 bg-opacity-75 transition-opacity" />
-        </Transition.Child>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-neutral-800 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
 
-        <div className="fixed z-10 inset-0 overflow-y-auto">
-          <div className="flex items-end sm:items-center justify-center min-h-full p-4 text-center sm:p-0">
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-              enterTo="opacity-100 translate-y-0 sm:scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            >
-              <Dialog.Panel className="relative bg-neutral-900 border-2 border-black px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full sm:p-6">
-                {children}
-              </Dialog.Panel>
-            </Transition.Child>
+          <div className="fixed z-10 inset-0 overflow-y-auto">
+            <div className="flex items-end sm:items-center justify-center min-h-full p-4 text-center sm:p-0">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                enterTo="opacity-100 translate-y-0 sm:scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              >
+                <Dialog.Panel className="relative bg-neutral-900 border-2 border-black px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full sm:p-6">
+                  {children}
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
           </div>
-        </div>
-      </Dialog>
-    </Transition.Root>
-  );
-}
+        </Dialog>
+      </Transition.Root>
+    );
+  },
+);
+
+export { Modal };
