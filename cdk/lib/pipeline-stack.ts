e3a5d8fb53eeb4818c37fa34d2e5229fb7266e00
Niall Maher <nialljoemaher@gmail.com>
2023-11-16 23:45:40 +0000

    Testing codeBuildDefaults

--- cdk/lib/pipeline-stack.ts
+++ cdk/lib/pipeline-stack.ts
@@ -1,6 +1,9 @@
 import * as cdk from "aws-cdk-lib";
 import type { Construct } from "constructs";
 import { AppStage } from "./app-stage";
+import { BuildEnvironmentVariableType } from "aws-cdk-lib/aws-codebuild";
+import { Secret } from "aws-cdk-lib/aws-secretsmanager";
+
 import {
   CodePipeline,
   CodePipelineSource,
@@ -24,16 +27,26 @@ export class PipelineStack extends cdk.Stack {
       synth: synthAction,
       dockerEnabledForSelfMutation: true,
       dockerEnabledForSynth: true,
+      codeBuildDefaults: {
+        buildEnvironment: {
+          environmentVariables: {
+            DATABASE_URL: {
+              type: BuildEnvironmentVariableType.SECRETS_MANAGER,
+              value: Secret.fromSecretNameV2(this, "DB_SECRET", "prod/db/url"),
+            },
+          },
+        },
+      },
     });
 
     const devAccountId = ssm.StringParameter.valueFromLookup(
       this,
-      `/env/dev/accountId`
+      `/env/dev/accountId`,
     );
 
     const prodAccountId = ssm.StringParameter.valueFromLookup(
       this,
-      `/env/prod/accountId`
+      `/env/prod/accountId`,
     );
 
     const defaultRegion = "eu-west-1";
