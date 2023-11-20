f90fd71233f9cea2894c97b2fb2d45829334ba64
Gary Byrne <garyb1@live.ie>
2023-11-20 18:33:41 +0000

    [Codú - #642] - Update theme provider (#643)

--- components/Theme/ThemeProvider.tsx
+++ components/Theme/ThemeProvider.tsx
@@ -1,14 +1,10 @@
 "use client";
 
-import type { ReactNode } from "react";
+import { type ReactNode } from "react";
 import { ThemeProvider } from "next-themes";
 
 const Providers = ({ children }: { children: ReactNode }) => {
-  return (
-    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-      {children}
-    </ThemeProvider>
-  );
+  return <ThemeProvider attribute="class">{children}</ThemeProvider>;
 };
 
 export default Providers;
