faffc4991b9f7952c00fb76dfc462b70d54e0655
Gary Byrne <garyb1@live.ie>
2023-11-20 18:12:19 +0000

    [Codú - #642] - Update theme provider

--- components/Theme/ThemeToggle.tsx
+++ components/Theme/ThemeToggle.tsx
@@ -1,21 +1,31 @@
 import { MoonIcon, SunIcon } from "@heroicons/react/solid";
 import { useTheme } from "next-themes";
 
-const ThemeToggle = () => {
-  const { theme, setTheme } = useTheme();
+const THEME_MODES = {
+  DARK: "dark",
+  LIGHT: "light",
+};
 
-  function toggleTheme() {
-    return theme === "light" ? setTheme("dark") : setTheme("light");
-  }
+const ThemeToggle = () => {
+  const { resolvedTheme, setTheme } = useTheme();
+
+  const toggleTheme = () => {
+    const newTheme =
+      resolvedTheme === THEME_MODES.DARK ? THEME_MODES.LIGHT : THEME_MODES.DARK;
+    setTheme(newTheme);
+  };
 
   return (
     <button
       onClick={toggleTheme}
+      aria-pressed={resolvedTheme === THEME_MODES.DARK}
       className="nav-button focus-style group relative flex-shrink-0 p-4 focus:ring-inset"
+      type="button"
+      title="Toggle dark mode"
     >
-      <SunIcon className="h-6 w-6 rotate-0 scale-100 text-neutral-500 transition-all group-hover:text-yellow-500 group-focus:text-yellow-500 dark:-rotate-90 dark:scale-0" />
-      <MoonIcon className="absolute left-1/2 top-1/2 h-6 w-6 -translate-x-1/2 -translate-y-1/2 rotate-90 scale-0 transform text-neutral-400 transition-all group-hover:text-white dark:rotate-0 dark:scale-100" />
-      <span className="sr-only">Toggle theme</span>
+      <span className="sr-only">Toggle Dark Mode</span>
+      <SunIcon className="h-6 w-6 rotate-0 scale-100 text-neutral-500 group-hover:text-yellow-500 group-focus:text-yellow-500 motion-safe:transition-all dark:-rotate-90 dark:scale-0" />
+      <MoonIcon className="absolute left-1/2 top-1/2 h-6 w-6 -translate-x-1/2 -translate-y-1/2 rotate-90 scale-0 transform text-neutral-400 group-hover:text-white motion-safe:transition-all dark:rotate-0 dark:scale-100" />
     </button>
   );
 };
