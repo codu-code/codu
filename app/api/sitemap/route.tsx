328a54c93ec744f0edfc7a378350a2f462de497f
Niall Maher <nialljoemaher@gmail.com>
2023-12-07 08:36:49 +0000

    Revert sitemap changes for logging

--- app/api/sitemap/route.tsx
+++ app/api/sitemap/route.tsx
@@ -1,5 +1,7 @@
 // Issue outstanding to easily pass secrets to docker in CDK securely so temporary workaround for build - https://github.com/aws/aws-cdk/issues/24691
+import { headers } from "next/headers";
 import db from "@/server/db/client";
+import * as Sentry from "@sentry/nextjs";
 
 const BASE_URL = "https://www.codu.co";
 const ROUTES_TO_INDEX = ["/articles", "/sponsorship", "/code-of-conduct"];
@@ -7,39 +9,47 @@ const ROUTES_TO_INDEX = ["/articles", "/sponsorship", "/code-of-conduct"];
 // @TODO - Lock down to codebuild/vercel domains after it works
 
 export async function GET() {
-  const articles = (
-    await db.post.findMany({
-      where: {
-        NOT: {
-          published: null,
+  try {
+    const headersList = headers();
+    const host = headersList.get("host");
+    const origin = headersList.get("origin");
+
+    Sentry.captureMessage(`HOST: ${host}, ORIGIN: ${origin}`);
+
+    const articles = (
+      await db.post.findMany({
+        where: {
+          NOT: {
+            published: null,
+          },
         },
-      },
-    })
-  ).map(({ slug, updatedAt }) => ({
-    url: `${BASE_URL}/articles/${slug}`,
-    lastModified: updatedAt,
-  }));
+      })
+    ).map(({ slug, updatedAt }) => ({
+      url: `${BASE_URL}/articles/${slug}`,
+      lastModified: updatedAt,
+    }));
 
-  const users = (await db.user.findMany()).map(({ username, updatedAt }) => ({
-    url: `${BASE_URL}/${username}`,
-    lastModified: updatedAt,
-  }));
+    const users = (await db.user.findMany()).map(({ username, updatedAt }) => ({
+      url: `${BASE_URL}/${username}`,
+      lastModified: updatedAt,
+    }));
 
-  const routes = ROUTES_TO_INDEX.map((route) => ({
-    url: BASE_URL + route,
-    lastModified: new Date(),
-  }));
-
-  const data = [
-    {
-      url: BASE_URL,
+    const routes = ROUTES_TO_INDEX.map((route) => ({
+      url: BASE_URL + route,
       lastModified: new Date(),
-      changeFrequency: "daily",
-    },
-    ...routes,
-    ...articles,
-    ...users,
-  ];
+    }));
+    const data = [
+      {
+        url: BASE_URL,
+        lastModified: new Date(),
+      },
+      ...routes,
+      ...articles,
+      ...users,
+    ];
 
-  return Response.json({ data });
+    return Response.json({ data });
+  } catch (error) {
+    Sentry.captureException(error);
+  }
 }
