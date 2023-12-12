ee9bec5913528e47f950d803d3fa27a020ca3233
tomcru <hello@tomrumpf.com>
2023-12-12 17:11:32 -0500

    fix: replace loaders to fix endless load

--- components/ProgressBar/ProgressBar.tsx
+++ components/ProgressBar/ProgressBar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import NextTopLoader from "nextjs-toploader";
+import HolyLoader from "holy-loader";
 import { usePathname, useSearchParams } from "next/navigation";
 import { useEffect } from "react";
 import { done as _done } from "nprogress";
@@ -13,10 +13,15 @@ const ProgressBar = () => {
   }, [pathname, searchParams]);
 
   return (
-    <NextTopLoader
+    <HolyLoader
       easing="linear"
-      showSpinner={false}
-      template='<div class="bar" role="bar"><div class="gradient"></div></div>'
+      color="linear-gradient(
+        to right,
+        rgb(251, 146, 60),
+        rgb(219, 39, 119)
+      )"
+      zIndex={50}
+      height="0.25rem"
     />
   );
 };
