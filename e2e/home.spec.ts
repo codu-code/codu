34265d941dd150ce70721e80c7f815df67edc2b3
Niall Maher <nialljoemaher@gmail.com>
2024-01-07 21:05:28 +0000

    Add e2e tests for articles and fix homepage test

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test";
 
 test.describe("Confirm homepage content", () => {
   test("Shared content", async ({ page }) => {
-    await page.goto("/");
+    await page.goto("http://localhost:3000/");
     // Check headers
 
     await expect(page.locator("h1")).toContainText("A space for coders");
@@ -18,7 +18,7 @@ test.describe("Confirm homepage content", () => {
   });
 
   test("Different devices", async ({ page, isMobile }) => {
-    await page.goto("/");
+    await page.goto("http://localhost:3000/");
 
     const elementVisible = await page
       .locator('text="Recommended topics"')
