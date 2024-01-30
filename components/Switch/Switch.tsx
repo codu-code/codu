c7f9f67ba9c01dfd0d2d1afdf6dd5ca013d7f1cc
Sơn Nguyễn <son@sonng.dev>
2024-01-30 19:30:09 +0000

    feat: change color of switch in light mode

--- components/Switch/Switch.tsx
+++ components/Switch/Switch.tsx
@@ -14,7 +14,7 @@ const Switch = React.forwardRef<
     {...props}
     ref={ref}
   >
-    <SwitchPrimitives.Thumb className="pointer-events-none ml-[2px] block h-5 w-5 rounded-full bg-black shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0 dark:bg-white" />
+    <SwitchPrimitives.Thumb className="pointer-events-none ml-[2px] block h-5 w-5 rounded-full bg-white shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0" />
   </SwitchPrimitives.Root>
 ));
 Switch.displayName = SwitchPrimitives.Root.displayName;
