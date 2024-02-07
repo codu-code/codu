86d458585ba854c3100138b13ac087dfe56ab531
Niall Maher <nialljoemaher@gmail.com>
2024-02-07 16:56:55 +0000

    Add Algolia index lambda and remove testCron lambda

---
+++ cdk/lambdas/algoliaIndex.js
@@ -0,0 +1,38 @@
+const { SSMClient, GetParameterCommand } = require("@aws-sdk/client-ssm");
+const ssmClient = new SSMClient({ region: "eu-west-1" });
+
+async function getSecretValue(secretName) {
+  const params = {
+    Name: secretName,
+    WithDecryption: true,
+  };
+
+  try {
+    const command = new GetParameterCommand(params);
+    const response = await ssmClient.send(command);
+    return response.Parameter.Value;
+  } catch (error) {
+    console.error(`Error retrieving secret: ${error}`);
+    throw error;
+  }
+}
+
+exports.handler = async function (event, context) {
+  console.log("Lambda running");
+
+  try {
+    const result = await getSecretValue("/env/algoliaIdx");
+    const parameterValue = result.Parameter.Value;
+    console.log("Parameter Value:", parameterValue);
+    // Use the parameter value as needed
+  } catch (error) {
+    console.error("Error fetching parameter:", error);
+    // Handle the error accordingly
+  }
+
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "text/json" },
+    body: "Hello world!",
+  };
+};
--- cdk/lib/cron-stack.ts
+++ cdk/lib/cron-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from "aws-cdk-lib";
 import * as targets from "aws-cdk-lib/aws-events-targets";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as events from "aws-cdk-lib/aws-events";
+import * as ssm from "aws-cdk-lib/aws-ssm";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import type { Construct } from "constructs";
 import * as path from "path";
@@ -12,10 +13,16 @@ export class CronStack extends cdk.Stack {
 
     const lambdaFn = new NodejsFunction(this, "Singleton", {
       timeout: cdk.Duration.seconds(120),
-      runtime: lambda.Runtime.NODEJS_18_X,
-      entry: path.join(__dirname, "/../lambdas/testCron.js"),
+      runtime: lambda.Runtime.NODEJS_20_X,
+      entry: path.join(__dirname, "/../lambdas/algoliaIndex.js"),
     });
 
+    const algoliaIdx = ssm.StringParameter.fromStringParameterName(
+      this,
+      "AlgoliaIdx",
+      "/env/algoliaIdx",
+    );
+    algoliaIdx.grantRead(lambdaFn);
     // 8:00 AM (UTC+0) on the first day of the month
     // See https://docs.aws.amazon.com/lambda/latest/dg/tutorial-scheduled-events-schedule-expressions.html
     const rule = new events.Rule(this, "Rule", {
