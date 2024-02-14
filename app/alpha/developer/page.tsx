9a912df18d2af454d8a084da42fa6639c2523026
John Paul Larkin <johnplarkin@gmail.com>
2024-02-14 13:25:22 +0000

    add splash/intro page

---
+++ app/alpha/developer/page.tsx
@@ -0,0 +1,74 @@
+import Link from "next/link";
+import React from "react";
+
+export default function Developer() {
+  return (
+    <>
+      <div className="relative bg-gray-900">
+        <div className="relative h-80 overflow-hidden bg-indigo-600 md:absolute md:left-0 md:h-full md:w-1/3 lg:w-1/2">
+          <img
+            className="h-full w-full object-cover"
+            src="https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=60&blend=6366F1&sat=-100&blend-mode=multiply"
+            alt=""
+          />
+          <svg
+            viewBox="0 0 926 676"
+            aria-hidden="true"
+            className="absolute -bottom-24 left-24 w-[57.875rem] transform-gpu blur-[118px]"
+          >
+            <path
+              fill="url(#60c3c621-93e0-4a09-a0e6-4c228a0116d8)"
+              fillOpacity=".4"
+              d="m254.325 516.708-90.89 158.331L0 436.427l254.325 80.281 163.691-285.15c1.048 131.759 36.144 345.144 168.149 144.613C751.171 125.508 707.17-93.823 826.603 41.15c95.546 107.978 104.766 294.048 97.432 373.585L685.481 297.694l16.974 360.474-448.13-141.46Z"
+            />
+            <defs>
+              <linearGradient
+                id="60c3c621-93e0-4a09-a0e6-4c228a0116d8"
+                x1="926.392"
+                x2="-109.635"
+                y1=".176"
+                y2="321.024"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop stopColor="#776FFF" />
+                <stop offset={1} stopColor="#FF4694" />
+              </linearGradient>
+            </defs>
+          </svg>
+        </div>
+        <div className="relative mx-auto max-w-7xl py-24 sm:py-32 lg:px-8 lg:py-40">
+          <div className="pl-6 pr-6 md:ml-auto md:w-2/3 md:pl-16 lg:w-1/2 lg:pl-24 lg:pr-0 xl:pl-32">
+            <h2 className="text-base font-semibold leading-7 text-indigo-400">
+              Are you ready?
+            </h2>
+
+            <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">
+              Discover Career Opportunities
+            </p>
+
+            <div className="mt-6 text-base leading-7 text-gray-300">
+              <p>
+                At Codu, we often receive inquiries from employers seeking
+                developers to contribute to their latest projects.
+              </p>
+
+              <p className="pt-4">
+                Our aim is to connect talented developers with exciting
+                opportunities in the ever-evolving tech landscape.
+              </p>
+            </div>
+
+            <div className="mt-8 flex justify-end">
+              <Link
+                href="/alpha/developer/sign-up"
+                className="flex w-[6rem] justify-center rounded-md bg-red-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-red-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+              >
+                Sign me up
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
