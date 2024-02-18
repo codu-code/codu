dc482e55b4522093a0e95a01a2c1c69f538d5fb4
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2024-02-19 00:09:29 +0900

    Patch/heading styles (#735)
    * feat: add new extension to disable text formatting to headings
    
    * chore: remove unused imports and import new extension
    
    * feat: disable bubble menu if heading is selected
    
    * patch: fix incorrect shortcut for strikethrough
    
    ---------
    
    Co-authored-by: Niall Maher <niall@codu.ie>

---
+++ components/editor/editor/extensions/disable-heading-text-style-shortcuts.tsx
@@ -0,0 +1,17 @@
+import { Extension } from "@tiptap/react";
+
+const DisableHeadingTextStyleShortcuts = Extension.create({
+  addKeyboardShortcuts() {
+    // If the selected node is a heading, disable the text style shortcuts
+    return {
+      'Mod-b': () => this.editor.isActive("heading") ?? this.editor.commands.toggleBold(),
+      'Mod-i': () => this.editor.isActive("heading") ?? this.editor.commands.toggleItalic(),
+      'Mod-u': () => this.editor.isActive("heading") ?? this.editor.commands.toggleUnderline(),
+      'Mod-e': () => this.editor.isActive("heading") ?? this.editor.commands.toggleCode(),
+      'Mod-shift-s': () => this.editor.isActive("heading") ?? this.editor.commands.toggleStrike(),
+      'Mod-shift-h': () => this.editor.isActive("heading") ?? this.editor.commands.toggleHighlight(),
+    };
+  },
+})
+
+export default DisableHeadingTextStyleShortcuts;
