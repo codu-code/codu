a4d6e996c003e6eb85104707adcaa39b8e45a9c9
Niall Maher <nialljoemaher@gmail.com>
2024-02-18 16:17:12 +0000

    Add iam role and policy to read from ssm

--- cdk/lib/cron-stack.ts
+++ cdk/lib/cron-stack.ts
@@ -3,6 +3,7 @@ import * as targets from "aws-cdk-lib/aws-events-targets";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as events from "aws-cdk-lib/aws-events";
 import * as ssm from "aws-cdk-lib/aws-ssm";
+import * as iam from "aws-cdk-lib/aws-iam";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import type { Construct } from "constructs";
 import * as path from "path";
@@ -10,6 +11,24 @@ import * as path from "path";
 export class CronStack extends cdk.Stack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
+
+    // Define the IAM role for the Lambda function
+    const lambdaRole = new iam.Role(this, "LambdaExecutionRole", {
+      assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
+    });
+
+    // Grant read access to SSM parameters
+    const policy = new iam.PolicyStatement({
+      actions: [
+        "ssm:GetParameter*",
+        "ssm:DescribeParameters",
+        "ssm:GetParameters",
+        "ssm:GetParametersByPath",
+      ],
+      resources: ["*"],
+    });
+
+    lambdaRole.addToPolicy(policy);
 
     const lambdaFn = new NodejsFunction(this, "AlgoliaLambda", {
       timeout: cdk.Duration.seconds(120),
@@ -19,18 +38,13 @@ export class CronStack extends cdk.Stack {
         __dirname,
         "/../lambdas/algoliaIndex/package-lock.json",
       ),
+      role: lambdaRole,
       bundling: {
         nodeModules: ["@aws-sdk/client-ssm", "algoliasearch", "pg"],
       },
     });
 
-    const algoliaIdx = ssm.StringParameter.fromStringParameterName(
-      this,
-      "AlgoliaIdx",
-      "/env/algoliaIdx",
-    );
-    algoliaIdx.grantRead(lambdaFn);
-    // 8:00 AM (UTC+0) on the first day of the month
+    // 6:00 (am) every day
     // See https://docs.aws.amazon.com/lambda/latest/dg/tutorial-scheduled-events-schedule-expressions.html
     const rule = new events.Rule(this, "Rule", {
       schedule: events.Schedule.expression("cron(0 6 * * ? *)"),
