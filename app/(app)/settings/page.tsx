5ca1b291e0d14f56143308fb71c9e600589f0943
John Paul Larkin <johnplarkin@gmail.com>
2024-03-09 21:15:26 +0000

    clean username on account creation

--- app/(app)/settings/page.tsx
+++ app/(app)/settings/page.tsx
@@ -30,12 +30,31 @@ export default async function Page() {
     redirect("/get-started");
   }
 
-  const user = await prisma.user.findUnique({
+  let user = await prisma.user.findUnique({
     where: {
       id: session.user.id,
     },
     select,
   });
+
+  const userName = user?.username;
+  if (userName) {
+    const cleanedUserName = userName
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "");
+
+    if (userName !== cleanedUserName) {
+      user = await prisma.user.update({
+        where: {
+          id: session.user.id,
+        },
+        data: {
+          username: cleanedUserName,
+        },
+        select,
+      });
+    }
+  }
 
   if (!user?.username) {
     const nanoid = customAlphabet("1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ", 3);
