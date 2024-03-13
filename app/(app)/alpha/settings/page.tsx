c870ede1058484f2b70176186f54ff15c6f5fc65
John Paul Larkin <107807466+John-Paul-Larkin@users.noreply.github.com>
2024-03-13 15:29:04 +0000

    Copy settings page to alpha directory for refactoring (#832)
    * Copy settings page to alpha directory for refactoring
    
    * copied profile.ts zod schema to alpha/settings
    
    * Split Zod schema into two parts and incorporate additional fields

---
+++ app/(app)/alpha/settings/page.tsx
@@ -0,0 +1,78 @@
+import { notFound, redirect } from "next/navigation";
+import Content from "./_client";
+import { getServerAuthSession } from "@/server/auth";
+import { customAlphabet } from "nanoid";
+import prisma from "@/server/db/client";
+import * as Sentry from "@sentry/nextjs";
+import { isUserSubscribedToNewsletter } from "@/server/lib/newsletter";
+
+export const metadata = {
+  title: "Settings - Update your profile",
+};
+
+// @TODO - Loading state for this page
+
+export default async function Page() {
+  const session = await getServerAuthSession();
+
+  const select = {
+    name: true,
+    username: true,
+    bio: true,
+    location: true,
+    websiteUrl: true,
+    emailNotifications: true,
+    newsletter: true,
+    image: true,
+  };
+
+  if (!session || !session.user) {
+    redirect("/get-started");
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: session.user.id,
+    },
+    select,
+  });
+
+  if (!user?.username) {
+    const nanoid = customAlphabet("1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ", 3);
+
+    const userName = session.user.name || "";
+    const cleanedUserName = userName
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "");
+    const initialUsername =
+      `${cleanedUserName.replace(/\s/g, "-")}-${nanoid()}`.toLowerCase();
+
+    const newUser = await prisma.user.update({
+      where: {
+        id: session.user.id,
+      },
+      data: {
+        username: initialUsername,
+      },
+      select,
+    });
+
+    return <Content profile={newUser} />;
+  }
+
+  if (!session.user.email) {
+    return notFound();
+  }
+
+  try {
+    const newsletter = await isUserSubscribedToNewsletter(session.user.email);
+    const cleanedUser = {
+      ...user,
+      newsletter,
+    };
+    return <Content profile={cleanedUser} />;
+  } catch (error) {
+    Sentry.captureException(error);
+    return <Content profile={user} />;
+  }
+}
