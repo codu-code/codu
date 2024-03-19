31d264e7af67c81c729e262bb7438eda48155edd
Shaun O'Neill <shaunoneill7@gmail.com>
2024-03-19 19:38:06 +0000

    Update the code of conduct page to better support dark theme (#852)

--- app/(app)/code-of-conduct/page.tsx
+++ app/(app)/code-of-conduct/page.tsx
@@ -7,18 +7,18 @@ export const metadata = {
 const CodeOfConduct = () => {
   return (
     <div>
-      <main className="relative  bg-white">
+      <main className="relative bg-white p-4 shadow-lg dark:border-neutral-600 dark:border-l-pink-600 dark:bg-neutral-900">
         <div className="lg:max-w-8xl relative px-4 pb-16 pt-12 sm:px-6 sm:pt-16 lg:mx-auto lg:px-8 ">
           <div className="lg:pl-8">
             <div className="mx-auto max-w-prose text-base  ">
-              <h1 className="mt-2 border-b-2 pb-8 text-3xl font-extrabold leading-8 tracking-tight text-black sm:text-6xl">
+              <h1 className="mt-2 border-b-2 pb-8 text-3xl font-extrabold leading-8 tracking-tight text-black dark:text-white sm:text-6xl">
                 Code Of Conduct
               </h1>
 
-              <h2 className="mb-4 mt-12 text-xl font-semibold leading-6 tracking-wide text-black">
+              <h2 className="mb-4 mt-12 text-xl font-semibold leading-6 tracking-wide text-black dark:text-white">
                 Support us
               </h2>
-              <p className="mt-3 text-lg text-neutral-500">
+              <p className="mt-3 text-lg text-neutral-500 dark:text-white">
                 We as members, contributors, and leaders pledge to make
                 participation in our community a harassment-free experience for
                 everyone, regardless of age, body size, visible or invisible
@@ -27,19 +27,19 @@ const CodeOfConduct = () => {
                 status, nationality, personal appearance, race, caste, color,
                 religion, or sexual identity and orientation.
               </p>
-              <p className="mt-3 text-lg text-neutral-500">
+              <p className="mt-3 text-lg text-neutral-500 dark:text-white">
                 We pledge to act and interact in ways that contribute to an
                 open, welcoming, diverse, inclusive, and healthy community.
               </p>
 
-              <h2 className="mb-4 mt-12 text-xl font-semibold leading-6 tracking-wide text-black">
+              <h2 className="mb-4 mt-12 text-xl font-semibold leading-6 tracking-wide text-black dark:text-white">
                 Our Standards
               </h2>
-              <p className="mt-3 text-lg text-neutral-500">
+              <p className="mt-3 text-lg text-neutral-500 dark:text-white">
                 Examples of behavior that contributes to a positive environment
                 for our community include:
               </p>
-              <ul className="mt-3 list-disc pl-8 text-lg text-neutral-500">
+              <ul className="mt-3 list-disc pl-8 text-lg text-neutral-500 dark:text-white">
                 <li className="my-4">
                   Demonstrating empathy and kindness toward other people
                 </li>
@@ -60,10 +60,10 @@ const CodeOfConduct = () => {
                 </li>
               </ul>
 
-              <p className="mt-3 text-lg text-neutral-500">
+              <p className="mt-3 text-lg text-neutral-500 dark:text-white">
                 Examples of unacceptable behavior include:
               </p>
-              <ul className="mt-3 list-disc pl-8 text-lg text-neutral-500">
+              <ul className="mt-3 list-disc pl-8 text-lg text-neutral-500 dark:text-white">
                 <li className="my-4">
                   The use of sexualized language or imagery, and sexual
                   attention or advances of any kind
@@ -83,16 +83,16 @@ const CodeOfConduct = () => {
                 </li>
               </ul>
 
-              <h2 className="mb-4 mt-12 text-xl font-semibold leading-6 tracking-wide text-black">
+              <h2 className="mb-4 mt-12 text-xl font-semibold leading-6 tracking-wide text-black dark:text-white">
                 Enforcement Responsibilities
               </h2>
-              <p className="mt-3 text-lg text-neutral-500">
+              <p className="mt-3 text-lg text-neutral-500 dark:text-white">
                 Project maintainers are responsible for clarifying and enforcing
                 our standards of acceptable behavior and will take appropriate
                 and fair corrective action in response to any behavior that they
                 deem inappropriate, threatening, offensive, or harmful.
               </p>
-              <p className="mt-3 text-lg text-neutral-500">
+              <p className="mt-3 text-lg text-neutral-500 dark:text-white">
                 Project maintainers have the right and responsibility to remove,
                 edit, or reject comments, commits, code, wiki edits, issues, and
                 other contributions that are not aligned to this Code of
@@ -100,10 +100,10 @@ const CodeOfConduct = () => {
                 when appropriate.
               </p>
 
-              <h2 className="mb-4 mt-12 text-xl font-semibold leading-6 tracking-wide text-black">
+              <h2 className="mb-4 mt-12 text-xl font-semibold leading-6 tracking-wide text-black dark:text-white">
                 Scope
               </h2>
-              <p className="mt-3 text-lg text-neutral-500">
+              <p className="mt-3 text-lg text-neutral-500 dark:text-white">
                 This Code of Conduct applies within all community spaces, and
                 also applies when an individual is officially representing the
                 community in public spaces. Examples of representing our
@@ -112,10 +112,10 @@ const CodeOfConduct = () => {
                 representative at an online or offline event.
               </p>
 
-              <h2 className="mb-4 mt-12 text-xl font-semibold leading-6 tracking-wide text-black">
+              <h2 className="mb-4 mt-12 text-xl font-semibold leading-6 tracking-wide text-black dark:text-white">
                 Enforcement
               </h2>
-              <p className="mt-3 text-lg text-neutral-500">
+              <p className="mt-3 text-lg text-neutral-500 dark:text-white">
                 Instances of abusive, harassing, or otherwise unacceptable
                 behavior may be reported to the project team responsible for
                 enforcement at{" "}
@@ -129,10 +129,10 @@ const CodeOfConduct = () => {
                 fairly.
               </p>
 
-              <h2 className="mb-4 mt-12 text-xl font-semibold leading-6 tracking-wide text-black">
+              <h2 className="mb-4 mt-12 text-xl font-semibold leading-6 tracking-wide text-black dark:text-white">
                 Attribution
               </h2>
-              <p className="mt-3 text-lg text-neutral-500">
+              <p className="mt-3 text-lg text-neutral-500 dark:text-white">
                 This Code of Conduct is adapted from the{" "}
                 <a
                   className="text-1xl z-20 bg-gradient-to-r from-orange-400 to-pink-600 bg-clip-text font-bold text-transparent"
