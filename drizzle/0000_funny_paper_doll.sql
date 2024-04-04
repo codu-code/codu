d6a60d9759472ad2576e5ccec5ef00756b3f90ac
JohnAllenTech <46611809+JohnAllenTech@users.noreply.github.com>
2024-04-04 21:19:42 +0100

    uncommented initial migration and updated seed script

--- drizzle/0000_funny_paper_doll.sql
+++ drizzle/0000_funny_paper_doll.sql
@@ -1,6 +1,6 @@
 -- Current sql file was generated after introspecting the database
 -- If you want to run this migration please uncomment this code before executing migrations
-/*
+
 DO $$ BEGIN
  CREATE TYPE "Role" AS ENUM('MODERATOR', 'ADMIN', 'USER');
 EXCEPTION
@@ -531,5 +531,3 @@ DO $$ BEGIN
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
-
-*/
