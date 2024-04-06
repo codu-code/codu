58edaa51374763e1c59606f17e0a73d6fe0eb515
JohnAllenTech <46611809+JohnAllenTech@users.noreply.github.com>
2024-04-06 21:34:01 +0100

    chore: added fully qualified table names to sql migration

--- drizzle/0002_add-likes-to-posts.sql
+++ drizzle/0002_add-likes-to-posts.sql
@@ -1,7 +1,7 @@
 /* Custom SQL to set value of Post.likes correctly for each post */
-UPDATE "Post"
+UPDATE public."Post"
 SET likes = (
     SELECT COUNT(*)
     FROM public."Like"
-    WHERE public."Like"."postId" = "Post".id
+    WHERE public."Like"."postId" = public."Post".id
  );
