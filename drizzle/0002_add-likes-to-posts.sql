bbecfc7a60b061330405e66aec570c7af6e95737
Niall Maher <nialljoemaher@gmail.com>
2024-04-09 11:54:36 +0200

    Revert "Adding Post.likes migration (#887)" (#889)
    This reverts commit b9967074999924b29468abef68d4bba7b587ec41.

--- drizzle/0002_add-likes-to-posts.sql
+++
@@ -1,7 +0,0 @@
-/* Custom SQL to set value of Post.likes correctly for each post */
-UPDATE public."Post"
-SET likes = (
-    SELECT COUNT(*)
-    FROM public."Like"
-    WHERE public."Like"."postId" = public."Post".id
- );
