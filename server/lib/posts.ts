d3d7dd901208ff57926deeec66db35d0ed2a8b05
JohnAllenTech <46611809+JohnAllenTech@users.noreply.github.com>
2024-04-09 23:24:00 +0100

    chore: migrated frontpage prisma calls to drizzle

--- server/lib/posts.ts
+++ server/lib/posts.ts
@@ -1,7 +1,10 @@
-import db from "@/server/db/client";
+import { db } from "@/server/db/index";
+
 import * as Sentry from "@sentry/nextjs";
 import "server-only";
 import { z } from "zod";
+import { bookmark, post, user } from "../db/schema";
+import { eq, and, isNotNull, lte, desc } from "drizzle-orm";
 
 export const GetPostSchema = z.object({
   slug: z.string(),
@@ -18,9 +21,8 @@ export async function getPost({ slug }: GetPost) {
   try {
     GetPostSchema.parse({ slug });
 
-    const response = await db.post.findUnique({
-      where: { slug },
-      select: {
+    const response = await db.query.post.findFirst({
+      columns: {
         id: true,
         title: true,
         body: true,
@@ -31,26 +33,21 @@ export async function getPost({ slug }: GetPost) {
         excerpt: true,
         canonicalUrl: true,
         showComments: true,
+      },
+      where: (posts, { eq }) => eq(posts.slug, slug),
+      with: {
+        bookmarks: { columns: { userId: true } },
+        tags: {
+          columns: { id: true },
+          with: { tag: { columns: { title: true } } },
+        },
         user: {
-          select: {
+          columns: {
             name: true,
             image: true,
             username: true,
             bio: true,
             id: true,
-          },
-        },
-        bookmarks: {
-          select: { userId: true },
-        },
-        tags: {
-          select: {
-            id: true,
-            tag: {
-              select: {
-                title: true,
-              },
-            },
           },
         },
       },
@@ -59,9 +56,9 @@ export async function getPost({ slug }: GetPost) {
     if (!response || response.published === null) {
       return null;
     }
-    const currentUserLikesPost = !!response.bookmarks.length;
+    const currentUserBookmarkedPost = !!response.bookmarks.length;
     response.bookmarks = [];
-    return { ...response, currentUserLikesPost };
+    return { ...response, currentUserBookmarkedPost };
   } catch (error) {
     Sentry.captureException(error);
     throw new Error("Error fetching post");
@@ -73,40 +70,45 @@ export async function getTrending({ currentUserId }: GetTrending) {
     GetTrendingSchema.parse({ currentUserId });
     const TRENDING_COUNT = 5;
 
-    const response = await db.post.findMany({
-      where: {
-        NOT: {
-          published: null,
-        },
-      },
-      select: {
-        id: true,
-        title: true,
-        updatedAt: true,
-        readTimeMins: true,
-        slug: true,
-        excerpt: true,
-        user: {
-          select: { name: true, image: true, username: true },
-        },
-        bookmarks: {
-          select: { userId: true },
-          where: { userId: currentUserId },
-        },
-      },
-      take: 20,
-      orderBy: {
-        likes: {
-          _count: "desc",
-        },
-      },
-    });
+    const bookmarked = db
+      .select()
+      .from(bookmark)
+      // if user not logged in just default to searching for "" as user which will always result in post not being bookmarked
+      // TODO figure out a way to skip this entire block if user is not logged in
+      .where(eq(bookmark.userId, currentUserId || ""))
+      .as("bookmarked");
 
-    const cleaned = response.map((post) => {
-      let currentUserLikesPost = !!post.bookmarks.length;
-      if (currentUserId === undefined) currentUserLikesPost = false;
-      post.bookmarks = [];
-      return { ...post, currentUserLikesPost };
+    const response = await db
+      .select({
+        post: {
+          id: post.id,
+          slug: post.slug,
+          title: post.title,
+          excerpt: post.excerpt,
+          published: post.published,
+          readTimeMins: post.readTimeMins,
+          likes: post.likes,
+          updatedAt: post.updatedAt,
+        },
+        bookmarked: { id: bookmarked.id },
+        user: { name: user.name, username: user.username, image: user.image },
+      })
+      .from(post)
+      .leftJoin(user, eq(post.userId, user.id))
+      .leftJoin(bookmarked, eq(bookmarked.postId, post.id))
+      .where(
+        and(
+          isNotNull(post.published),
+          lte(post.published, new Date().toISOString()),
+        ),
+      )
+      .limit(20)
+      .orderBy(desc(post.likes));
+
+    const cleaned = response.map((elem) => {
+      let currentUserBookmarkedPost = !!elem.bookmarked;
+      if (currentUserId === undefined) currentUserBookmarkedPost = false;
+      return { ...elem.post, user: elem.user, currentUserBookmarkedPost };
     });
 
     const shuffled = cleaned.sort(() => 0.5 - Math.random());
