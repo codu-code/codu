ec3d782658faf672a22a42c8dec909040753237a
John Paul Larkin <107807466+John-Paul-Larkin@users.noreply.github.com>
2024-08-15 10:43:18 +0100

    add index.ts for centralised export of catalyst components

---
+++ app/(app)/alpha/additional-details/tailwind-catalyts/index.ts
@@ -0,0 +1,6 @@
+export { ErrorMessage, Field, Label } from "./fieldset";
+export { Heading, Subheading } from "./heading";
+export { Divider } from "./divider";
+export { Input } from "./input";
+export { Select } from "./select";
+export { Button } from "./button";
