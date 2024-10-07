ad66dd4220f0b7929ce4b6126874ddc305c416a0
Praveen Kumar <praveen@quizizz.com>
2024-10-07 22:23:31 +0530

    feat: inuit e2e workflow setup

--- playwright.config.ts
+++ playwright.config.ts
@@ -71,7 +71,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "npm run dev",
+    command: "npm run dev:e2e",
     url: "http://127.0.0.1:3000",
     reuseExistingServer: !process.env.CI,
   },
