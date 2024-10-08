f45f7b43d9cc07645e31c2cee82b837a564ca6aa
Niall Maher <nialljoemaher@gmail.com>
2024-10-08 22:05:18 +0100

    Update a bunch of non-breaking packages. (#1096)
    * Update a bunch of non-breaking packages.
    
    * Updates file formatting for tailwind prettier bump

--- components/ui/Search.tsx
+++ components/ui/Search.tsx
@@ -542,7 +542,7 @@ export function MobileSearch({
     <div className="contents lg:hidden">
       <button
         type="button"
-        className="focus-style relative flex-shrink-0 rounded-md  p-2 text-neutral-500 hover:bg-neutral-200 hover:text-neutral-600 dark:text-neutral-400 dark:hover:bg-neutral-900 dark:hover:text-white"
+        className="focus-style relative flex-shrink-0 rounded-md p-2 text-neutral-500 hover:bg-neutral-200 hover:text-neutral-600 dark:text-neutral-400 dark:hover:bg-neutral-900 dark:hover:text-white"
         aria-label="Search the site"
         {...buttonProps}
       >
