fbed02d8e0450c85a952a48063d8dcd2c7fcb36e
JohnAllenTech <46611809+JohnAllenTech@users.noreply.github.com>
2024-10-14 12:17:34 -0600

    chore: replaced order of tests to see if cookies are cleared at a test level or a run level

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -1,18 +1,6 @@
 import { test, expect } from "@playwright/test";
 
 test.describe("Testing homepage views", () => {
-  test("Unauthenticated homepage view", async ({ page }) => {
-    await page.context().clearCookies();
-    await page.goto("http://localhost:3000/");
-    // Check headers
-
-    await expect(page.locator("h1")).toContainText("A space for coders");
-    await expect(page.locator("h1")).not.toContainText("Unwanted text");
-    await expect(page.locator("h2")).toContainText(
-      "Sign up today to become a writer and get a free invite to our Discord community",
-    );
-  });
-
   test("Authenticated homepage view", async ({ page, isMobile }) => {
     await page.goto("http://localhost:3000/");
 
@@ -25,6 +13,16 @@ test.describe("Testing homepage views", () => {
           name: "Your Posts",
         }),
       ).toBeVisible();
+  });
+  test("Unauthenticated homepage view", async ({ page }) => {
+    await page.context().clearCookies();
+    await page.goto("http://localhost:3000/");
+
+    await expect(page.locator("h1")).toContainText("A space for coders");
+    await expect(page.locator("h1")).not.toContainText("Unwanted text");
+    await expect(page.locator("h2")).toContainText(
+      "Sign up today to become a writer and get a free invite to our Discord community",
+    );
   });
 
   test("Authenticated landing page view", async ({ page, isMobile }) => {
