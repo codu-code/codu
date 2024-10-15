9803fd13cd2d6c764858322ff05016715cf2119b
John <46611809+JohnAllenTech@users.noreply.github.com>
2024-10-15 00:35:43 -0600

    Feat/improving existing e2e tests (#1125)
    * chore: improved current E2E tests and enhanced labels
    
    * chore: added more tests around article page and unauth/auth views

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -1,23 +1,59 @@
 import { test, expect } from "playwright/test";
 import "dotenv/config";
 
-test.describe("Login Page", () => {
-  test("should display the welcome message", async ({ page }) => {
-    await page.goto("http://localhost:3000/get-started");
-    const welcomeMessage = page.getByText("Sign in or create your accounttton");
-    expect(welcomeMessage).toBeTruthy();
-  });
-  test("should display the Github login button", async ({ page }) => {
+test.describe("Unauthenticated Login Page", () => {
+  test.beforeEach(async ({ page }) => {
     await page.context().clearCookies();
     await page.goto("http://localhost:3000/get-started");
-    await page.waitForTimeout(3000);
+  });
+  test("Sign up page contains sign up links", async ({ page, isMobile }) => {
+    await expect(page.getByText("CodúBetaSign in or create")).toBeVisible();
+    await expect(
+      page.getByRole("heading", { name: "Sign in or create your account" }),
+    ).toBeVisible();
+    await expect(page.getByRole("link", { name: "return home" })).toBeVisible();
+    if (!isMobile) {
+      await expect(
+        page.getByRole("button", { name: "Sign up for free" }),
+      ).toBeVisible();
+      await expect(
+        page.getByRole("button", { name: "Sign in", exact: true }),
+      ).toBeVisible();
+    }
+  });
+  test("Login page contains GitHub button", async ({ page }) => {
     await expect(page.getByTestId("github-login-button")).toBeVisible();
   });
 
-  test("should display the Gitlab login button", async ({ page }) => {
-    await page.context().clearCookies();
-    await page.goto("http://localhost:3000/get-started");
-    await page.waitForLoadState();
+  test("Login page contains GitLab button", async ({ page }) => {
     await expect(page.getByTestId("gitlab-login-button")).toBeVisible();
+  });
+});
+
+test.describe("Authenticated Login Page", () => {
+  test("Sign up page contains sign up links", async ({ page, isMobile }) => {
+    // authenticated users are kicked back to the homepage if they try to go to /get-started
+    await page.goto("http://localhost:3000/get-started");
+    expect(page.url()).toEqual("http://localhost:3000/");
+    await expect(page.getByText("CodúBetaSign in or create")).toBeHidden();
+    await expect(
+      page.getByRole("heading", { name: "Sign in or create your account" }),
+    ).toBeHidden();
+    await expect(page.getByRole("link", { name: "return home" })).toBeHidden();
+    if (!isMobile) {
+      await expect(
+        page.getByRole("button", { name: "Sign up for free" }),
+      ).toBeHidden();
+      await expect(
+        page.getByRole("button", { name: "Sign in", exact: true }),
+      ).toBeHidden();
+    }
+  });
+  test("Login page contains GitHub button", async ({ page }) => {
+    await expect(page.getByTestId("github-login-button")).toBeHidden();
+  });
+
+  test("Login page contains GitLab button", async ({ page }) => {
+    await expect(page.getByTestId("gitlab-login-button")).toBeHidden();
   });
 });
