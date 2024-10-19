6d9b19717540ff82e8cceea21ddccfe820b97194
JohnAllenTech <46611809+JohnAllenTech@users.noreply.github.com>
2024-10-19 11:27:53 -0600

    chore: removed flaky test from article suite

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,6 +29,9 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
   },
+  expect: {
+    timeout: 10000,
+  },
 
   /* Configure projects for major browsers */
   projects: [
