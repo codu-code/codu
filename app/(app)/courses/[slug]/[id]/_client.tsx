e5c4c25c687c4bf00c599c70e3b71d488fbf2338
Carolina Cobo <ccobo.dev@gmail.com>
2024-10-24 20:58:35 +0100

    WIP video player

--- app/(app)/courses/[slug]/[id]/_client.tsx
+++ app/(app)/courses/[slug]/[id]/_client.tsx
@@ -12,15 +12,55 @@ const Content = ({ session }: { session: Session | null }) => {
     notFound();
   }
 
+  const Content = () => {
+    return mockContentList.map(
+      ({
+        id,
+        title,
+        longDescription,
+        description,
+        watched,
+        publishedDate,
+        author,
+        imageLink,
+      }) => (
+        <div key={id} className="mx-auto w-full max-w-7xl py-8">
+          <h2 className="text-3xl font-extrabold tracking-tight text-neutral-800 dark:text-white">
+            {title}
+          </h2>
+          <li key={author} className="flex justify-between gap-x-6 py-5">
+            <div className="flex min-w-0 gap-x-4">
+              <img
+                alt=""
+                src={imageLink}
+                className="h-12 w-12 flex-none rounded-full bg-gray-50"
+              />
+              <div className="min-w-0 flex-auto">
+                <p className="leading-6text-neutral-800 text-sm font-semibold dark:text-white">
+                  {author}
+                </p>
+                <p className="mt-1 truncate text-xs leading-5 text-gray-500">
+                  {publishedDate}
+                </p>
+              </div>
+            </div>
+          </li>
+          <div>{description}</div>
+          <div>{longDescription}</div>
+        </div>
+      ),
+    )[0];
+  };
+
   return (
-    <div className="flex w-full flex-grow flex-col">
+    <div className="overflow-auto">
       <div className="w-full divide-x divide-gray-700 lg:grid lg:grid-cols-12">
         {/* Video container */}
         <div className="col-span-9">
-          <div className="bg-black">
+          <div className="mx-auto w-full max-w-7xl bg-black">
             <iframe
               className="aspect-video w-full"
-              src="https://www.youtube.com/embed/Q68wOyeG5lc"
+              src="https://www.youtube.com/embed/oKKG-CpDjrI"
               title="YouTube video"
             ></iframe>
           </div>
@@ -33,7 +73,7 @@ const Content = ({ session }: { session: Session | null }) => {
               mockContentList.map((item, index) => (
                 <li
                   key={index}
-                  className="flex flex-row items-center justify-between px-2  py-2 "
+                  className="flex flex-row items-center justify-between px-2 py-2"
                 >
                   <div className="flex flex-row items-center">
                     <SquarePlay
@@ -60,9 +100,8 @@ const Content = ({ session }: { session: Session | null }) => {
           </ul>
         </div>
       </div>
-      <div className="flex-none">
-        <div>Video title</div>
-        <div>descritpion</div>
+      <div className="mx-auto max-w-5xl">
+        <Content />
       </div>
     </div>
   );
