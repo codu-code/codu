02aabfeede9525919c1ee1af0cb0fdd0acbce80c
JohnAllenTech <46611809+JohnAllenTech@users.noreply.github.com>
2024-10-26 13:36:20 -0600

    chore: moving out from user auth out into a util

--- e2e/my-posts.spec.ts
+++ e2e/my-posts.spec.ts
@@ -1,14 +1,15 @@
 import test from "@playwright/test";
+import { loggedInAsUserOne } from "./utils";
 
 test.describe("Unauthenticated my-posts Page", () => {
-  test.beforeEach(async ({ page }) => {
-    await page.context().clearCookies();
-  });
   //
   // Replace with tests for unauthenticated users
 });
 
 test.describe("Authenticated my-posts Page", () => {
+  test.beforeEach(async ({ page }) => {
+    await loggedInAsUserOne(page);
+  });
   //
   // Replace with tests for authenticated users
 });
