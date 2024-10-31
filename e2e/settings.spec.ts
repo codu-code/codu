67c5c4f34dbe73656169e5f110f880d75e26087b
Peter Cruckshank <petecruckshank@gmail.com>
2024-10-31 19:19:00 -0400

    feat: UI tests for username change on /settings

--- e2e/settings.spec.ts
+++ e2e/settings.spec.ts
@@ -1,4 +1,4 @@
-import test from "@playwright/test";
+import { test, expect } from "@playwright/test";
 import { loggedInAsUserOne } from "./utils";
 
 test.describe("Unauthenticated setttings Page", () => {
@@ -10,6 +10,32 @@ test.describe("Authenticated settings Page", () => {
   test.beforeEach(async ({ page }) => {
     await loggedInAsUserOne(page);
   });
-  //
-  // Replace with tests for authenticated users
+  
+  // Test for changing username
+  test('Username input field', async ({ page }) => {
+    await page.goto('http://localhost:3000/settings', { timeout: 30000 });
+  
+    // Wait for the username input field to be visible
+    await page.locator('input[id="username"]').waitFor();
+  
+    // Test that the input field is visible and has the correct attributes
+    const inputField = page.locator('input[id="username"]');
+    await expect(inputField).toBeVisible();
+    await expect(inputField).toHaveAttribute('type', 'text');
+    await expect(inputField).toHaveAttribute('autocomplete', 'username');
+  
+    // Test that the error message appears when the input field is invalid
+    await inputField.fill('45&p^x#@!96%*()');
+    await page.locator('button[type="submit"]').click(); 
+    const errorMessage = page.locator('p:text-is("Username can only contain alphanumerics and dashes.")')
+    await expect(errorMessage).toBeVisible();
+    await expect(errorMessage).toHaveText('Username can only contain alphanumerics and dashes.');
+    // Reset the form 
+    await page.locator('button:has-text("Reset")').click();
+  
+    // Test that the input field can be filled with a valid value and saves it
+    await inputField.fill('codu-rules');
+    await page.locator('button[type="submit"]').click(); 
+    await expect(inputField).toHaveValue('codu-rules');
+  });
 });
