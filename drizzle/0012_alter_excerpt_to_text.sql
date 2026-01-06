f49bd90891fe453055d513b531aa206db07987f2
NiallJoeMaher <nialljoemaher@gmail.com>
2026-01-06 07:44:21 +0000

    chore(db): remove old incremental migrations

--- drizzle/0012_alter_excerpt_to_text.sql
+++
@@ -1,2 +0,0 @@
--- Alter excerpt column from varchar(300) to text to allow longer excerpts
-ALTER TABLE "AggregatedArticle" ALTER COLUMN "excerpt" TYPE text;
--- drizzle/0013_add_og_image_url.sql
+++
@@ -1,2 +0,0 @@
--- Add ogImageUrl column to AggregatedArticle for storing Open Graph images fetched from article URLs
-ALTER TABLE "AggregatedArticle" ADD COLUMN IF NOT EXISTS "ogImageUrl" text;
