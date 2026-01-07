a992efc1bbda971f7bde0893ceaf8aeee63447db
NiallJoeMaher <nialljoemaher@gmail.com>
2026-01-07 07:21:16 +0000

    refactor(ui): update link component and trending posts loading styles

--- components/TrendingPosts/TrendingPostsLoading.tsx
+++ components/TrendingPosts/TrendingPostsLoading.tsx
@@ -2,10 +2,12 @@ import { FeedItemLoading } from "@/components/Feed";
 
 function LoadingTrendingPosts() {
   return (
-    <div>
-      {Array.from({ length: 5 }, (_, i) => (
-        <FeedItemLoading key={i} />
-      ))}
+    <div className="relative md:col-span-7">
+      <section>
+        {Array.from({ length: 5 }, (_, i) => (
+          <FeedItemLoading key={i} />
+        ))}
+      </section>
     </div>
   );
 }
--- components/ui-components/link.tsx
+++ components/ui-components/link.tsx
@@ -1,22 +1,25 @@
-/**
- * TODO: Update this component to use your client-side framework's link
- * component. We've provided examples of how to do this for Next.js, Remix, and
- * Inertia.js in the Catalyst documentation:
- *
- * https://catalyst.tailwindui.com/docs#client-side-router-integration
- */
-
 import * as Headless from "@headlessui/react";
+import NextLink from "next/link";
 import React, { forwardRef } from "react";
 
 export const Link = forwardRef(function Link(
   props: { href: string } & React.ComponentPropsWithoutRef<"a">,
   ref: React.ForwardedRef<HTMLAnchorElement>,
 ) {
+  const isExternal =
+    props.href.startsWith("http") || props.href.startsWith("//");
+
+  if (isExternal) {
+    return (
+      <Headless.DataInteractive>
+        <a {...props} ref={ref} target="_blank" rel="noopener noreferrer" />
+      </Headless.DataInteractive>
+    );
+  }
+
   return (
     <Headless.DataInteractive>
-      {/* eslint-disable jsx-a11y/anchor-has-content  */}
-      <a {...props} ref={ref} />
+      <NextLink {...props} ref={ref} />
     </Headless.DataInteractive>
   );
 });
