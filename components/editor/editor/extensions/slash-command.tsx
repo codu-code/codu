83de791fc3afc1e197bfc8454fb977cd50b3bcc6
NiallJoeMaher <nialljoemaher@gmail.com>
2026-01-08 16:41:22 +0000

    feat(editor): add new PostEditor component and E2E test infrastructure
      - Add PostEditor with WriteTab and LinkTab for article/link creation
      - Add URL metadata fetching for link posts
      - Add article toolbar with formatting options
      - Add E2E editor tests and test utilities
      - Update E2E setup with link post test data
      - Update slash command and image upload extensions

--- components/editor/editor/extensions/slash-command.tsx
+++ components/editor/editor/extensions/slash-command.tsx
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+ 
 import React, {
   useState,
   useEffect,
@@ -231,11 +231,11 @@ const CommandList = ({
   range: any;
 }) => {
   // Use items.length as key to reset selectedIndex when items change
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+   
   const [selectedIndex, setSelectedIndex] = useState(0);
 
   // Reset selection when items change - using useLayoutEffect to run synchronously
-  // eslint-disable-next-line react-hooks/set-state-in-effect
+   
   useLayoutEffect(() => {
     setSelectedIndex(0);
   }, [items]);
